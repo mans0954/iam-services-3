c4845d9ba64f988032945f6f7a53b4474d36a653
Alexander Duckardt <aduckardt@gmail.com>
2012-10-26 21:36:14 +0400

    fix sql for km module

--- schema/schema-km/mysql/dml/update_data.sql
+++ schema/schema-km/mysql/dml/update_data.sql
@@ -1,7 +1,7 @@
 
 use openiam;
 
-insert into USERS (PASSWORD,FIRST_NAME, LAST_NAME, STATUS, COMPANY_ID, SYSTEM_FLAG ) values('0001','system','system','ACTIVE','100','1');
+insert into USERS (USER_ID,FIRST_NAME, LAST_NAME, STATUS, COMPANY_ID, SYSTEM_FLAG ) values('0001','system','system','ACTIVE','100','1');
 
 insert into LOGIN(SERVICE_ID, LOGIN, MANAGED_SYS_ID, USER_ID, PASSWORD,RESET_PWD, IS_LOCKED, AUTH_FAIL_COUNT) VALUES('IDM','system','0','0001','passwd00',0,0,0);
 
