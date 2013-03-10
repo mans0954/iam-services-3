12cea5448d995d070d7d6bc3d8a6daea2d7e36b7
lbornov2openiam <lev.bornovalov@openiam.com>
2013-03-10 18:07:25 -0300

    NameQualifier attribute for SAML Providers

---
+++ schema/schema-am/3.0/mysql/dml/10.name_qualifier_attrs.sql
@@ -0,0 +1,2 @@
+INSERT INTO AUTH_ATTRIBUTE (AUTH_ATTRIBUTE_ID, ATTRIBUTE_NAME, PROVIDER_TYPE, DESCRIPTION, REQUIRED, DATA_TYPE)
+	VALUES('NameQualifier', 'Name Qualifier', 'SAML_PROVIDER', 'Value of the NameQualifier attribute in the NameID element', 'N', 'singleValue');
