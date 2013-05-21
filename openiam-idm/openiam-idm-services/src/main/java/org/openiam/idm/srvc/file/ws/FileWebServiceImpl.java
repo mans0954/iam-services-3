06146c870635a5c07fb83b0c7a43873417c35ed2
Dmitry Zaporozhec <elpilasgsm@gmail.com>
2013-05-21 22:58:28 +0400

    MSys-Recon refactor

--- openiam-idm/openiam-idm-services/src/main/java/org/openiam/idm/srvc/file/ws/FileWebServiceImpl.java
+++ openiam-idm/openiam-idm-services/src/main/java/org/openiam/idm/srvc/file/ws/FileWebServiceImpl.java
@@ -20,69 +20,67 @@ import org.springframework.stereotype.Service;
 @Service("fileWebService")
 public class FileWebServiceImpl implements FileWebService {
 
-	@Value("${iam.files.location}")
-	private String absolutePath;
+    @Value("${iam.files.location}")
+    private String absolutePath;
 
-	private static final Log log = LogFactory.getLog(FileWebServiceImpl.class);
+    private static final Log log = LogFactory.getLog(FileWebServiceImpl.class);
 
-	@Override
-	@WebMethod
-	public String getFile(
-			@WebParam(name = "fileName", targetNamespace = "") String fName) {
-		try {
-			return this.get(fName);
-		} catch (Exception e) {
-			e.printStackTrace();
+    @Override
+    public String getFile(
+            @WebParam(name = "fileName", targetNamespace = "") String fName) {
+        try {
+            return this.get(fName);
+        } catch (Exception e) {
+            e.printStackTrace();
 
-		}
-		return null;
-	}
+        }
+        return null;
+    }
 
-	@Override
-	@WebMethod
-	public File saveFile(
-			@WebParam(name = "fileName", targetNamespace = "") String fName,
-			@WebParam(name = "fileContent", targetNamespace = "") String value) {
-		try {
-			return this.save(fName, value);
-		} catch (Exception e) {
-			e.printStackTrace();
+    @Override
+    public File saveFile(
+            @WebParam(name = "fileName", targetNamespace = "") String fName,
+            @WebParam(name = "fileContent", targetNamespace = "") String value) {
+        try {
+            return this.save(fName, value);
+        } catch (Exception e) {
+            e.printStackTrace();
 
-		}
-		return null;
-	}
+        }
+        return null;
+    }
 
-	private String readFile(File file) {
-		int ch;
-		StringBuffer strContent = new StringBuffer("");
-		FileInputStream fin = null;
-		try {
-			fin = new FileInputStream(file);
-			while ((ch = fin.read()) != -1)
-				strContent.append((char) ch);
-			fin.close();
-		} catch (Exception e) {
-			log.info(e);
-		}
-		return strContent.toString();
-	}
+    private String readFile(File file) {
+        int ch;
+        StringBuffer strContent = new StringBuffer("");
+        FileInputStream fin = null;
+        try {
+            fin = new FileInputStream(file);
+            while ((ch = fin.read()) != -1)
+                strContent.append((char) ch);
+            fin.close();
+        } catch (Exception e) {
+            log.info(e);
+        }
+        return strContent.toString();
+    }
 
-	private String get(String fName) throws Exception {
-		File file = new File(absolutePath + fName);
-		if (!file.exists()) {
-			log.info("FILE: " + absolutePath + fName + "NOT EXIST");
-			return null;
-		}
-		return readFile(file);
-	}
+    private String get(String fName) throws Exception {
+        File file = new File(absolutePath + fName);
+        if (!file.exists()) {
+            log.info("FILE: " + absolutePath + fName + "NOT EXIST");
+            return null;
+        }
+        return readFile(file);
+    }
 
-	private File save(String fName, String value) throws Exception {
-		FileWriter fw = new FileWriter(absolutePath + fName, false);
-		fw.append(value);
-		fw.flush();
-		fw.close();
+    private File save(String fName, String value) throws Exception {
+        FileWriter fw = new FileWriter(absolutePath + fName, false);
+        fw.append(value);
+        fw.flush();
+        fw.close();
 
-		return new File(absolutePath + fName);
-	}
+        return new File(absolutePath + fName);
+    }
 
 }
--- openiam-idm/openiam-idm-services/src/main/java/org/openiam/idm/srvc/recon/service/ReconciliationConfigDAOImpl.java
+++ openiam-idm/openiam-idm-services/src/main/java/org/openiam/idm/srvc/recon/service/ReconciliationConfigDAOImpl.java
@@ -17,93 +17,91 @@ import static org.hibernate.criterion.Example.create;
  */
 public class ReconciliationConfigDAOImpl implements ReconciliationConfigDAO {
 
-	private static final Log log = LogFactory
-			.getLog(ReconciliationConfigDAO.class);
+    private static final Log log = LogFactory
+            .getLog(ReconciliationConfigDAO.class);
 
-	private SessionFactory sessionFactory;
+    private SessionFactory sessionFactory;
 
-	
-	public void setSessionFactory(SessionFactory session) {
-		   this.sessionFactory = session;
-	}
+    public void setSessionFactory(SessionFactory session) {
+        this.sessionFactory = session;
+    }
 
-	protected SessionFactory getSessionFactory() {
-		try {
-			return (SessionFactory) new InitialContext()
-					.lookup("SessionFactory");
-		} catch (Exception e) {
-			log.error("Could not locate SessionFactory in JNDI", e);
-			throw new IllegalStateException(
-					"Could not locate SessionFactory in JNDI");
-		}
-	}
+    protected SessionFactory getSessionFactory() {
+        try {
+            return (SessionFactory) new InitialContext()
+                    .lookup("SessionFactory");
+        } catch (Exception e) {
+            log.error("Could not locate SessionFactory in JNDI", e);
+            throw new IllegalStateException(
+                    "Could not locate SessionFactory in JNDI");
+        }
+    }
 
-
-	public ReconciliationConfig add(ReconciliationConfig transientInstance) {
-		log.debug("persisting ReconiliationConfig instance");
-		try {
-			sessionFactory.getCurrentSession().persist(transientInstance);
-			log.debug("persist successful");
+    public ReconciliationConfig add(ReconciliationConfig transientInstance) {
+        log.debug("persisting ReconiliationConfig instance");
+        try {
+            sessionFactory.getCurrentSession().persist(transientInstance);
+            log.debug("persist successful");
             return transientInstance;
-		} catch (RuntimeException re) {
-			log.error("persist failed", re);
-			throw re;
-		}
-	}
+        } catch (RuntimeException re) {
+            log.error("persist failed", re);
+            throw re;
+        }
+    }
 
+    public void remove(ReconciliationConfig persistentInstance) {
+        log.debug("deleting ReconiliationConfig instance");
+        try {
+            sessionFactory.getCurrentSession().delete(persistentInstance);
+            log.debug("delete successful");
+        } catch (RuntimeException re) {
+            log.error("delete failed", re);
+            throw re;
+        }
+    }
 
-	public void remove(ReconciliationConfig persistentInstance) {
-		log.debug("deleting ReconiliationConfig instance");
-		try {
-			sessionFactory.getCurrentSession().delete(persistentInstance);
-			log.debug("delete successful");
-		} catch (RuntimeException re) {
-			log.error("delete failed", re);
-			throw re;
-		}
-	}
+    public ReconciliationConfig update(ReconciliationConfig detachedInstance) {
+        log.debug("merging ReconiliationConfig instance");
+        try {
+            ReconciliationConfig result = (ReconciliationConfig) sessionFactory
+                    .getCurrentSession().merge(detachedInstance);
+            log.debug("merge successful");
+            return result;
+        } catch (RuntimeException re) {
+            log.error("merge failed", re);
+            throw re;
+        }
+    }
 
-	public ReconciliationConfig update(ReconciliationConfig detachedInstance) {
-		log.debug("merging ReconiliationConfig instance");
-		try {
-			ReconciliationConfig result = (ReconciliationConfig) sessionFactory
-					.getCurrentSession().merge(detachedInstance);
-			log.debug("merge successful");
-			return result;
-		} catch (RuntimeException re) {
-			log.error("merge failed", re);
-			throw re;
-		}
-	}
-
-	public ReconciliationConfig findById(java.lang.String id) {
-		log.debug("getting ReconiliationConfig instance with id: " + id);
-		try {
-			ReconciliationConfig instance = (ReconciliationConfig) sessionFactory
-					.getCurrentSession()
-					.get(
-							"org.openiam.idm.srvc.recon.dto.ReconciliationConfig",
-							id);
-			if (instance == null) {
-				log.debug("get successful, no instance found");
-			} else {
-				log.debug("get successful, instance found");
-			}
-			return instance;
-		} catch (RuntimeException re) {
-			log.error("get failed", re);
-			throw re;
-		}
-	}
+    public ReconciliationConfig findById(java.lang.String id) {
+        log.debug("getting ReconiliationConfig instance with id: " + id);
+        try {
+            ReconciliationConfig instance = (ReconciliationConfig) sessionFactory
+                    .getCurrentSession()
+                    .get("org.openiam.idm.srvc.recon.dto.ReconciliationConfig",
+                            id);
+            if (instance == null) {
+                log.debug("get successful, no instance found");
+            } else {
+                log.debug("get successful, instance found");
+            }
+            return instance;
+        } catch (RuntimeException re) {
+            log.error("get failed", re);
+            throw re;
+        }
+    }
 
     public ReconciliationConfig findByResourceId(java.lang.String resourceId) {
-     try {
+        try {
             Session session = sessionFactory.getCurrentSession();
-            Query qry = session.createQuery("from org.openiam.idm.srvc.recon.dto.ReconciliationConfig rc " +
-                    " where rc.resourceId = :resourceId ");
+            Query qry = session
+                    .createQuery("from org.openiam.idm.srvc.recon.dto.ReconciliationConfig rc "
+                            + " where rc.resourceId = :resourceId ");
             qry.setString("resourceId", resourceId);
 
-            List<ReconciliationConfig> config = (List<ReconciliationConfig>) qry.list();
+            List<ReconciliationConfig> config = (List<ReconciliationConfig>) qry
+                    .list();
 
             if (config != null && !config.isEmpty()) {
                 return config.get(0);
@@ -116,23 +114,19 @@ public class ReconciliationConfigDAOImpl implements ReconciliationConfigDAO {
         }
     }
 
-
-
     public void removeByResourceId(java.lang.String resourceId) {
         try {
 
-        	Session session = sessionFactory.getCurrentSession();
-		    Query qry = session.createQuery("delete org.openiam.idm.srvc.recon.dto.ReconciliationConfig rc " +
-					" where rc.resourceId = :resourceId ");
-		    qry.setString("resourceId", resourceId);
-		    qry.executeUpdate();
-         } catch (HibernateException re) {
+            Session session = sessionFactory.getCurrentSession();
+            Query qry = session
+                    .createQuery("delete org.openiam.idm.srvc.recon.dto.ReconciliationConfig rc "
+                            + " where rc.resourceId = :resourceId ");
+            qry.setString("resourceId", resourceId);
+            qry.executeUpdate();
+        } catch (HibernateException re) {
             log.error("delete failed", re);
             throw re;
         }
     }
 
-
-
 }
-
