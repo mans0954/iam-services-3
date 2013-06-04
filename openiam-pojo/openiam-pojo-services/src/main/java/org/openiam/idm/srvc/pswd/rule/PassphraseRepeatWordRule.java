679dbebac0b504d640971ac3e9e05b70dd6f1502
ektaagarwal <ekta.agarwal@openiam.com>
2013-06-04 15:40:30 +0530

    IDMAPPS-681 Added support for passphrases

---
+++ openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/pswd/rule/PassphraseRepeatWordRule.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2009, OpenIAM LLC 
+ * This file is part of the OpenIAM Identity and Access Management Suite
+ *
+ *   OpenIAM Identity and Access Management Suite is free software: 
+ *   you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License 
+ *   version 3 as published by the Free Software Foundation.
+ *
+ *   OpenIAM is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   Lesser GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenIAM.  If not, see <http://www.gnu.org/licenses/>. *
+ */
+
+/**
+ * 
+ */
+package org.openiam.idm.srvc.pswd.rule;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.StringTokenizer;
+
+import org.openiam.idm.srvc.policy.dto.PolicyAttribute;
+import org.openiam.idm.srvc.pswd.dto.PasswordValidationCode;
+
+/**
+ * Validates a password to ensure that the repetition of words in passphrase is
+ * as defined in the password policy
+ * 
+ * @author Ekta
+ * 
+ */
+public class PassphraseRepeatWordRule extends AbstractPasswordRule {
+
+	public PasswordValidationCode isValid() {
+		PasswordValidationCode retval = PasswordValidationCode.SUCCESS;
+		boolean enabled = false;
+
+		PolicyAttribute attribute = policy
+				.getAttribute("REPEAT_SAME_WORD_PASSPHRASE");
+		if (attribute.getValue1() != null && attribute.getValue1().length() > 0) {
+			enabled = Boolean.parseBoolean(attribute.getValue1());
+
+		}
+		if (!enabled){
+			// should not allow repetition of words in passphrase
+			StringTokenizer tokenizer = new StringTokenizer(password);
+			List<String> words = new ArrayList<String>();
+			while (tokenizer.hasMoreTokens()){
+				words.add(tokenizer.nextToken().toUpperCase());
+			}
+			//for faster search
+			Collections.sort(words);
+			for (int i=0; i< words.size() -1; i++){
+				//only one comparison needed, as list is sorted
+				if (words.get(i).equalsIgnoreCase(words.get(i+1))){
+					retval = PasswordValidationCode.FAIL_MIN_WORDS_PASSPHRASE_RULE;
+					break;
+				}
+			}
+			
+			
+		}
+		return retval;
+	}
+
+}
