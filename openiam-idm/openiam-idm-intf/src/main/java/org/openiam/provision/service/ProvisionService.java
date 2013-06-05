fcc46e21d6a55dd7b4fdd781ca39f74cd0a64453
Dmitry Zaporozhec <elpilasgsm@gmail.com>
2013-06-06 00:34:22 +0400

    attributesList for connectors

--- openiam-idm/openiam-idm-intf/src/main/java/org/openiam/provision/service/ProvisionService.java
+++ openiam-idm/openiam-idm-intf/src/main/java/org/openiam/provision/service/ProvisionService.java
@@ -36,146 +36,129 @@ import org.openiam.provision.dto.ProvisionUser;
 import org.openiam.provision.resp.LookupUserResponse;
 import org.openiam.provision.resp.PasswordResponse;
 import org.openiam.provision.resp.ProvisionUserResponse;
-
+import org.openiam.spml2.msg.LookupAttributeRequestType;
 
 /**
- * <code>ProvisionService</code> Interface for the Provisioning service which is used for provisioning users.
+ * <code>ProvisionService</code> Interface for the Provisioning service which is
+ * used for provisioning users.
+ * 
  * @author suneet
- *
+ * 
  */
-@WebService(targetNamespace = "http://www.openiam.org/service/provision", name="ProvisionControllerService")
+@WebService(targetNamespace = "http://www.openiam.org/service/provision", name = "ProvisionControllerService")
 public interface ProvisionService {
 
     /**
-     * Operation validates the connection information that was supplied for this managed system
+     * Operation validates the connection information that was supplied for this
+     * managed system
+     * 
      * @param managedSysId
      * @return
      */
     @WebMethod
-	public Response testConnectionConfig(
-			@WebParam(name = "managedSysId", targetNamespace = "")
-			String managedSysId);
+    public Response testConnectionConfig(
+            @WebParam(name = "managedSysId", targetNamespace = "") String managedSysId);
 
+    /**
+     * The addUser operation enables a requestor to create a new user on the
+     * target systems
+     */
+    @WebMethod
+    public ProvisionUserResponse addUser(
+            @WebParam(name = "user", targetNamespace = "") ProvisionUser user)
+            throws Exception;
 
-	/**
-	 * The addUser operation enables a requestor to create a new user on the target systems
-	 */
-	@WebMethod
-	public ProvisionUserResponse addUser(
-			@WebParam(name = "user", targetNamespace = "")
-			ProvisionUser user) throws Exception;
-	
-	/**
-	 * The modifyUser operation enables the requestor to modify an existing user in appropriate target systems
-	 */
-	@WebMethod
-	public ProvisionUserResponse modifyUser(
-			@WebParam(name = "user", targetNamespace = "")
-			ProvisionUser user);
-	
-		
-	
-	/**
-	 * The deleteUser operation enables the requestor to delete an existing user from the appropriate target systems
-	 */
-	@WebMethod
-	public ProvisionUserResponse deleteUser(
-			@WebParam(name = "securityDomain", targetNamespace = "")
-			String securityDomain,
-			@WebParam(name = "managedSystemId", targetNamespace = "")
-			String managedSystemId, 
-			@WebParam(name = "principal", targetNamespace = "")
-			String principal,
-			@WebParam(name="status", targetNamespace="")
-			UserStatusEnum status,
-			@WebParam(name="requestorId", targetNamespace="")
-			String requestorId);
+    /**
+     * The modifyUser operation enables the requestor to modify an existing user
+     * in appropriate target systems
+     */
+    @WebMethod
+    public ProvisionUserResponse modifyUser(
+            @WebParam(name = "user", targetNamespace = "") ProvisionUser user);
 
-	@WebMethod
-	public ProvisionUserResponse deleteByUserId(
-			@WebParam(name = "user", targetNamespace = "")
-			ProvisionUser user,
-			@WebParam(name="status", targetNamespace="")
-			UserStatusEnum status,
-			@WebParam(name="requestorId", targetNamespace="")
-			String requestorId);
+    /**
+     * The deleteUser operation enables the requestor to delete an existing user
+     * from the appropriate target systems
+     */
+    @WebMethod
+    public ProvisionUserResponse deleteUser(
+            @WebParam(name = "securityDomain", targetNamespace = "") String securityDomain,
+            @WebParam(name = "managedSystemId", targetNamespace = "") String managedSystemId,
+            @WebParam(name = "principal", targetNamespace = "") String principal,
+            @WebParam(name = "status", targetNamespace = "") UserStatusEnum status,
+            @WebParam(name = "requestorId", targetNamespace = "") String requestorId);
 
+    @WebMethod
+    public ProvisionUserResponse deleteByUserId(
+            @WebParam(name = "user", targetNamespace = "") ProvisionUser user,
+            @WebParam(name = "status", targetNamespace = "") UserStatusEnum status,
+            @WebParam(name = "requestorId", targetNamespace = "") String requestorId);
 
     @WebMethod
     public ProvisionUserResponse deprovisionSelectedResources(
-            @WebParam(name = "userId", targetNamespace = "")
-            String userId,
-            @WebParam(name = "requestorUserId", targetNamespace = "")
-            String requestorUserId,
-            @WebParam(name="resourceList", targetNamespace="")
-            List<String> resourceList);
-	
-	/**
-	 * The setPassword operation enables a requestor to specify a new password for an user across target systems
-	 * @param passwordSync
-	 * @return
-	 */
-	@WebMethod
-	public Response setPassword(
-			@WebParam(name = "passwordSync", targetNamespace = "")
-			PasswordSync passwordSync);
+            @WebParam(name = "userId", targetNamespace = "") String userId,
+            @WebParam(name = "requestorUserId", targetNamespace = "") String requestorUserId,
+            @WebParam(name = "resourceList", targetNamespace = "") List<String> resourceList);
+
+    /**
+     * The setPassword operation enables a requestor to specify a new password
+     * for an user across target systems
+     * 
+     * @param passwordSync
+     * @return
+     */
+    @WebMethod
+    public Response setPassword(
+            @WebParam(name = "passwordSync", targetNamespace = "") PasswordSync passwordSync);
 
     public Response syncPasswordFromSrc(
-			@WebParam(name = "passwordSync", targetNamespace = "")
-			PasswordSync passwordSync);
+            @WebParam(name = "passwordSync", targetNamespace = "") PasswordSync passwordSync);
 
-	@WebMethod
-	public PasswordResponse resetPassword(
-			@WebParam(name = "passwordSync", targetNamespace = "")
-			PasswordSync passwordSync);
-	
+    @WebMethod
+    public PasswordResponse resetPassword(
+            @WebParam(name = "passwordSync", targetNamespace = "") PasswordSync passwordSync);
 
-	
-	/**
-	 * Operation locks or unlocks an account.  If the operation flag is true, then the user is locked. Otherwise
-	 * its is unlocked.
-	 * @param userId
-	 * @param operation
-	 * @return
-	 */
-	@WebMethod
-	Response lockUser(
-			@WebParam(name = "userId", targetNamespace = "")
-			String userId, 
-			@WebParam(name = "operation", targetNamespace = "")
-			AccountLockEnum operation,
-			@WebParam(name = "requestorId", targetNamespace = "")
-			String requestorId);
-	
-	/**
-	 * Operation disables or un-disables an account.  If the operation flag is true, then the user is disabled. Otherwise
-	 * its is disabled.
-	 * @param userId
-	 * @param operation
-	 * @return
-	 */
-	@WebMethod
-	Response disableUser(
-			@WebParam(name = "userId", targetNamespace = "")
-			String userId, 
-			@WebParam(name = "operation", targetNamespace = "")
-			boolean operation,
-			@WebParam(name = "requestor", targetNamespace = "")
-			String requestorId);
+    /**
+     * Operation locks or unlocks an account. If the operation flag is true,
+     * then the user is locked. Otherwise its is unlocked.
+     * 
+     * @param userId
+     * @param operation
+     * @return
+     */
+    @WebMethod
+    Response lockUser(
+            @WebParam(name = "userId", targetNamespace = "") String userId,
+            @WebParam(name = "operation", targetNamespace = "") AccountLockEnum operation,
+            @WebParam(name = "requestorId", targetNamespace = "") String requestorId);
+
+    /**
+     * Operation disables or un-disables an account. If the operation flag is
+     * true, then the user is disabled. Otherwise its is disabled.
+     * 
+     * @param userId
+     * @param operation
+     * @return
+     */
+    @WebMethod
+    Response disableUser(
+            @WebParam(name = "userId", targetNamespace = "") String userId,
+            @WebParam(name = "operation", targetNamespace = "") boolean operation,
+            @WebParam(name = "requestor", targetNamespace = "") String requestorId);
 
     @WebMethod
     LookupUserResponse getTargetSystemUser(
-            @WebParam(name = "principalName", targetNamespace = "")
-            String principalName,
-            @WebParam(name = "managedSysId", targetNamespace = "")
-            String managedSysId);
+            @WebParam(name = "principalName", targetNamespace = "") String principalName,
+            @WebParam(name = "managedSysId", targetNamespace = "") String managedSysId);
 
     @WebMethod
     LookupUserResponse getTargetSystemUserWithUserId(
-            @WebParam(name = "userId", targetNamespace = "")
-            String userId,
-            @WebParam(name = "managedSysId", targetNamespace = "")
-            String managedSysId);
+            @WebParam(name = "userId", targetNamespace = "") String userId,
+            @WebParam(name = "managedSysId", targetNamespace = "") String managedSysId);
 
+    @WebMethod
+    public List<String> getAttributesList(
+            @WebParam(name = "managedSysId", targetNamespace = "") String managedSysId,
+            @WebParam(name = "config", targetNamespace = "") LookupAttributeRequestType config);
 
 }
