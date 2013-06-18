96c4ef4d140589c62be00583ef2084d894a87c49
zhuk <alexander.zhuk@openiam.com>
2013-06-18 19:50:55 +0300

    Fixed groovy scripts for provisioning

--- openiam-idm/idm-scripts/provision/ad/win02/sAMAccountName-NP.groovy
+++ openiam-idm/idm-scripts/provision/ad/win02/sAMAccountName-NP.groovy
@@ -1,10 +1,8 @@
-if("EXIST" == targetSystemIdentityStatus) {
-    output = null
-    return output
-}
+
 def attr = user.getUserAttributes().get("sAMAccountName")
+
 if (attr?.value) {
-    output = attr.value
+    output =  attr.value
 }else {
     output = user.employeeId
 }
