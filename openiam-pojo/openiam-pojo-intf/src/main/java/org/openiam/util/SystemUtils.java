6a8a95fb7506243cfbf07b721dcfbd1564839941
zhuk <alexander.zhuk@openiam.com>
2013-06-21 20:34:42 +0300

    Added System Utils helper

---
+++ openiam-pojo/openiam-pojo-intf/src/main/java/org/openiam/util/SystemUtils.java
@@ -0,0 +1,11 @@
+package org.openiam.util;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: root
+ * Date: 6/21/13
+ * Time: 6:16 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SystemUtils {
+}
