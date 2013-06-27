3d11b84f74296316ac653e45139a2886a3bda6b5
lbornov2 <lbornov2@lbornov2-Aspire-V3-771.(none)>
2013-06-28 00:06:53 +0200

    Finalize fixing of User<->Resource bug fix

--- openiam-bpm/openiam-bpm-activiti/src/main/java/org/openiam/bpm/activiti/delegate/entitlements/EntitleUserToResource.java
+++ openiam-bpm/openiam-bpm-activiti/src/main/java/org/openiam/bpm/activiti/delegate/entitlements/EntitleUserToResource.java
@@ -27,9 +27,6 @@ public class EntitleUserToResource extends AbstractEntitlementsDelegate {
 	@Autowired
 	private ResourceService resourceService;
 
-    @Autowired
-    private ResourceUserDAO resourceUserDAO;
-
 	public EntitleUserToResource() {
 		super();
 	}
@@ -39,14 +36,15 @@ public class EntitleUserToResource extends AbstractEntitlementsDelegate {
 		final String resourceId = (String)execution.getVariable(ActivitiConstants.ASSOCIATION_ID);
 		final String userId = (String)execution.getVariable(ActivitiConstants.MEMBER_ASSOCIATION_ID);	
 		
-		final User user = userDataService.getUserDto(userId);
+		User user = userDataService.getUserDto(userId);
 		final ResourceEntity entity = resourceService.findResourceById(resourceId);
-		if(user != null && entity != null) {
+		if(user != null && entity != null && resourceService.getResourceUser(userId, resourceId) == null) {
             final ResourceUserEntity toSave = new ResourceUserEntity();
             toSave.setUserId(userId);
             toSave.setResourceId(resourceId);
-            resourceUserDAO.save(toSave);
+            resourceService.save(toSave);
 
+            user = userDataService.getUserDto(userId);
 			final ProvisionUser pUser = new ProvisionUser(user);
 			final UserResourceAssociation association = new UserResourceAssociation();
             association.setOperation(AttributeOperationEnum.ADD);
