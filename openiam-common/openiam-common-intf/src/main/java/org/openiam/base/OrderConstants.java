54f5c233b40ed83e2858abc4ffadbb66ee24ba67
zhuk <alexander.zhuk@openiam.com>
2013-08-03 15:24:42 +0300

    [IDMAPPS-794, IDMAPPS-797] Policy ordered in alphabetical order

---
+++ openiam-common/openiam-common-intf/src/main/java/org/openiam/base/OrderConstants.java
@@ -0,0 +1,11 @@
+package org.openiam.base;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: root
+ * Date: 8/3/13
+ * Time: 12:34 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OrderConstants {
+}
---
+++ openiam-common/openiam-common-intf/src/main/java/org/openiam/idm/searchbeans/AttributeMapSearchBean.java
@@ -0,0 +1,11 @@
+package org.openiam.idm.searchbeans;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: root
+ * Date: 8/3/13
+ * Time: 12:15 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class AttributeMapSearchBean {
+}
