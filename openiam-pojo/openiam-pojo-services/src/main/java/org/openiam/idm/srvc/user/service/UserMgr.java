3ac108279ac604cdd92f07c8f7fc94d5b7e40691
Vitaly Yakunin <vitaly.yakunin@gmail.com>
2013-08-28 18:20:08 +0200

    IDMAPPS-886

--- openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/user/service/UserMgr.java
+++ openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/user/service/UserMgr.java
@@ -285,8 +285,12 @@ public class UserMgr implements UserDataService {
                     if (entity != null) {
                         userEntity.getEmailAddresses().remove(entity);
                     }
-                } else if (e.getOperation().equals(AttributeOperationEnum.ADD)) {
-                    EmailAddressEntity entity = emailAddressDozerConverter.convertToEntity(e, false);
+                } else if (e.getOperation().equals(AttributeOperationEnum.ADD) || e.getOperation().equals(AttributeOperationEnum.REPLACE)) {
+                    EmailAddressEntity entity = emailAddressDao.findById(e.getEmailId());
+                    if(entity != null) {
+                        emailAddressDao.evict(entity);
+                    }
+                    entity = emailAddressDozerConverter.convertToEntity(e, false);
                     entity.setParent(userEntity);
                     userEntity.getEmailAddresses().add(entity);
                 } else if (e.getOperation().equals(AttributeOperationEnum.REPLACE)) {
