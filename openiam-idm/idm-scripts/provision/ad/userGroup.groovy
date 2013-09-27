eb25b84cc14ddc152b936f35ca933ba10e273dfb
zhuk <alexander.zhuk@openiam.com>
2013-09-27 11:21:14 +0300

    Fixed script userGroup

--- openiam-idm/idm-scripts/provision/ad/userGroup.groovy
+++ openiam-idm/idm-scripts/provision/ad/userGroup.groovy
@@ -1,3 +1,4 @@
+import org.openiam.base.AttributeOperationEnum
 import org.openiam.base.BaseAttribute
 import org.openiam.base.BaseAttributeContainer
 import org.openiam.idm.srvc.grp.dto.Group
@@ -6,13 +7,18 @@ import org.openiam.idm.srvc.role.dto.Role
 
 def groupBaseDN = ",OU=idm-test,DC=ad,DC=openiamdemo,DC=info"
 
+def oldGroupSet = userBeforeModify.groups as Set
 def groupSet = user.groups as Set
 
-//user.roles?.each { Role r-> groupSet.addAll(r.groups) } //TODO: uncomment this if groups from roles needed
+// user.roles?.each { Role r-> groupSet.addAll(r.groups) } //TODO: uncomment this if groups from roles needed
+// userBeforeModify.roles?.each { Role r-> oldGroupSet.addAll(r.groups) } //TODO: uncomment this if groups from roles needed
 
 def attributeContainer = new BaseAttributeContainer()
 output = null
 groupSet?.each { Group g->
+    if (!(g in oldGroupSet)) {
+        g.operation = AttributeOperationEnum.ADD
+    }
     println("Adding group id  " + g.grpId + " --> " + (g.grpName + groupBaseDN))
     def qualifiedGroupName = "cn=" + g.grpName + groupBaseDN
     attributeContainer.attributeList.add(new BaseAttribute(qualifiedGroupName, qualifiedGroupName, g.operation))
