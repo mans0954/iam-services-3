f22861f9974de3b130fec03434f8e7ff8c39c7bf
zhuk <alexander.zhuk@openiam.com>
2013-10-10 16:05:22 +0300

    Fixed groovy script

---
+++ openiam-idm/idm-scripts/provision/ad/userGroup.groovy
@@ -0,0 +1,57 @@
+import org.openiam.base.AttributeOperationEnum
+import org.openiam.base.BaseAttribute
+import org.openiam.base.BaseAttributeContainer
+import org.openiam.dozer.converter.GroupDozerConverter
+import org.openiam.idm.srvc.grp.dto.Group
+import org.openiam.idm.srvc.role.dto.Role
+import org.openiam.idm.srvc.role.service.RoleDataService
+
+def groupBaseDN = "," + matchParam.baseDn
+
+def oldGroupSet = (binding.hasVariable("userBeforeModify")) ? userBeforeModify.groups : []
+def groupSet = user.groups as Set
+
+if (binding.hasVariable("userBeforeModify")) {
+    oldGroupSet.addAll(getGroupsFromRoles(userBeforeModify.roles))
+}
+groupSet.addAll(getGroupsFromRoles(user.roles))
+
+def attributeContainer = new BaseAttributeContainer()
+output = null
+groupSet?.each { Group g->
+    if (!g.managedSysId || (binding.hasVariable("managedSysId") && (g.managedSysId == managedSysId))) { // filter by managed sys
+        if (!g.companyId || (binding.hasVariable("org") && (g.companyId == org?.id))) { // filter by organization
+            if (!(g in oldGroupSet)) {
+                g.operation = AttributeOperationEnum.ADD
+            }
+            println("Adding group id  " + g.grpId + " --> " + (g.grpName + groupBaseDN))
+            def qualifiedGroupName = "cn=" + g.grpName + groupBaseDN
+            attributeContainer.attributeList.add(new BaseAttribute(qualifiedGroupName, qualifiedGroupName, g.operation))
+        }
+    }
+}
+oldGroupSet?.each { Group g->
+    if (!g.managedSysId || (binding.hasVariable("managedSysId") && (g.managedSysId == managedSysId))) { // filter by managed sys
+        if (!g.companyId || (binding.hasVariable("org") && (g.companyId == org?.id))) { // filter by organization
+            if (!(g in groupSet)) {
+                g.operation = AttributeOperationEnum.DELETE
+                println("Deleting group id  " + g.grpId + " --> " + (g.grpName + groupBaseDN))
+                def qualifiedGroupName = "cn=" + g.grpName + groupBaseDN
+                attributeContainer.attributeList.add(new BaseAttribute(qualifiedGroupName, qualifiedGroupName, g.operation))
+            }
+        }
+    }
+}
+if (attributeContainer.attributeList) {
+    output = attributeContainer
+}
+
+Set<Group> getGroupsFromRoles(Set<Role> roles) {
+    def roleDataService = context?.getBean("roleDataService") as RoleDataService
+    def groupDozerConverter = context?.getBean("groupDozerConverter") as GroupDozerConverter
+    def groups = [] as Set
+    roles.each {Role r->
+        groups.addAll(groupDozerConverter.convertToDTOSet(roleDataService.getRole(r.roleId).groups, false))
+    }
+    return groups
+}
