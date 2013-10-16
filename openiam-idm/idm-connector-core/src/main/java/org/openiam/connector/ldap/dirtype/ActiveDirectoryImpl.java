863b613a2558b61d2e73655fa8779ee4abbf6442
zhuk <alexander.zhuk@openiam.com>
2013-10-16 13:30:14 +0300

    Fixed NPE

--- openiam-idm/idm-connector-core/src/main/java/org/openiam/connector/ldap/dirtype/ActiveDirectoryImpl.java
+++ openiam-idm/idm-connector-core/src/main/java/org/openiam/connector/ldap/dirtype/ActiveDirectoryImpl.java
@@ -1,5 +1,6 @@
 package org.openiam.connector.ldap.dirtype;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openiam.base.AttributeOperationEnum;
@@ -219,7 +220,7 @@ public class ActiveDirectoryImpl implements Directory {
                 }
             }
         }
-        if (supervisorMembershipList != null) {
+        if (CollectionUtils.isNotEmpty(supervisorMembershipList)) {
 
             String identityDN = matchObj.getKeyField() + "=" + identity + "," + matchObj.getBaseDn();
 
