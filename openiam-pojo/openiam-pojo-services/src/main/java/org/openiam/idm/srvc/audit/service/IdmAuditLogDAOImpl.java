88a38445f8d029d49197a46ff2944880c54aea63
zhuk <alexander.zhuk@openiam.com>
2013-12-03 18:23:08 +0300

    Rolled back IdmAuditLog DAO

--- openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/audit/service/IdmAuditLogDAOImpl.java
+++ openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/audit/service/IdmAuditLogDAOImpl.java
@@ -4,7 +4,6 @@ package org.openiam.idm.srvc.audit.service;
 
 import org.apache.commons.lang.StringUtils;
 import org.hibernate.Criteria;
-import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.openiam.core.dao.BaseDaoImpl;
 import org.openiam.idm.searchbeans.AuditLogSearchBean;
@@ -14,76 +13,50 @@ import org.openiam.idm.srvc.searchbean.converter.AuditLogSearchBeanConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.*;
-
-import static org.hibernate.criterion.Projections.rowCount;
-
 /**
  * RDMBS implementation the DAO for IdmAudit
- * @author Suneet Shah 
+ * @author Suneet Shah
  */
 @Repository("idmAuditLogDAO")
 public class IdmAuditLogDAOImpl extends BaseDaoImpl<IdmAuditLogEntity, String> implements IdmAuditLogDAO {
-	
-	@Autowired
-	private AuditLogSearchBeanConverter converter;
-	
 
-	protected Criteria getExampleCriteriaWithoutOrder(final IdmAuditLogEntity entity) {
-    	final Criteria criteria = super.getCriteria();
-    	if(entity != null) {
-    		if(StringUtils.isNotBlank(entity.getId())) {
-    			criteria.add(Restrictions.eq(getPKfieldName(), entity.getId()));
-    		}
+    @Autowired
+    private AuditLogSearchBeanConverter converter;
+
+
+    @Override
+    protected Criteria getExampleCriteria(final IdmAuditLogEntity entity) {
+        final Criteria criteria = super.getCriteria();
+        if(entity != null) {
+            if(StringUtils.isNotBlank(entity.getId())) {
+                criteria.add(Restrictions.eq(getPKfieldName(), entity.getId()));
+            }
             if(StringUtils.isNotBlank(entity.getAction())) {
                 criteria.add(Restrictions.eq("action",entity.getAction()));
             }
-    	}
-
-    	return criteria;
-	}
-
-    protected Criteria getExampleCriteria(final IdmAuditLogEntity entity) {
-        final Criteria criteria = getExampleCriteriaWithoutOrder(entity);
-        criteria.addOrder(Order.desc("timestamp"));
+        }
         return criteria;
     }
 
-    public int count(AuditLogSearchBean searchBean) {
+    @Override
+    protected Criteria getExampleCriteria(SearchBean searchBean) {
         Criteria criteria = super.getCriteria();
         if(searchBean != null && (searchBean instanceof AuditLogSearchBean)) {
-            criteria = getExampleCriteriaWithoutOrder(converter.convert(searchBean));
-            if(searchBean.getFrom() != null && searchBean.getTo() != null) {
-                criteria.add(Restrictions.between("timestamp", searchBean.getFrom(), searchBean.getTo()));
-            } else if(searchBean.getFrom() != null) {
-                criteria.add(Restrictions.gt("timestamp", searchBean.getFrom()));
-            } else if(searchBean.getTo() != null) {
-                criteria.add(Restrictions.lt("timestamp", searchBean.getTo()));
+            final AuditLogSearchBean auditSearch = (AuditLogSearchBean)searchBean;
+            criteria = getExampleCriteria(converter.convert(auditSearch));
+
+            if(auditSearch.getFrom() != null && auditSearch.getTo() != null) {
+                criteria.add(Restrictions.between("timestamp", auditSearch.getFrom(), auditSearch.getTo()));
+            } else if(auditSearch.getFrom() != null) {
+                criteria.add(Restrictions.gt("timestamp", auditSearch.getFrom()));
+            } else if(auditSearch.getTo() != null) {
+                criteria.add(Restrictions.lt("timestamp", auditSearch.getTo()));
             }
         }
-        return ((Number) criteria.setProjection(rowCount())
-                .uniqueResult()).intValue();
+        return criteria;
     }
 
-	@Override
-	protected Criteria getExampleCriteria(SearchBean searchBean) {
-		Criteria criteria = super.getCriteria();
-		if(searchBean != null && (searchBean instanceof AuditLogSearchBean)) {
-			final AuditLogSearchBean auditSearch = (AuditLogSearchBean)searchBean;
-			criteria = getExampleCriteria(converter.convert(auditSearch));
-			
-			if(auditSearch.getFrom() != null && auditSearch.getTo() != null) {
-				criteria.add(Restrictions.between("timestamp", auditSearch.getFrom(), auditSearch.getTo()));
-			} else if(auditSearch.getFrom() != null) {
-				criteria.add(Restrictions.gt("timestamp", auditSearch.getFrom()));
-			} else if(auditSearch.getTo() != null) {
-				criteria.add(Restrictions.lt("timestamp", auditSearch.getTo()));
-			}
-		}
-		return criteria;
-	}
-
-	@Override
+    @Override
     protected String getPKfieldName() {
         return "id";
     }
