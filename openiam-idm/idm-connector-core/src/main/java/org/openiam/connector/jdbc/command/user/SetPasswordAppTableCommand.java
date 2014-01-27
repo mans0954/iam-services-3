6615aacfdba8411689ade0741d000d8fb00310b3
Dmitry Zaporozhec <elpilasgsm@gmail.com>
2014-01-27 22:28:41 +0400

    check java formatter

--- openiam-idm/idm-connector-core/src/main/java/org/openiam/connector/jdbc/command/user/SetPasswordAppTableCommand.java
+++ openiam-idm/idm-connector-core/src/main/java/org/openiam/connector/jdbc/command/user/SetPasswordAppTableCommand.java
@@ -1,17 +1,17 @@
 package org.openiam.connector.jdbc.command.user;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import org.openiam.connector.jdbc.command.base.AbstractAppTableCommand;
+import org.openiam.connector.jdbc.command.data.AppTableConfiguration;
 import org.openiam.connector.type.ConnectorDataException;
 import org.openiam.connector.type.constant.ErrorCode;
 import org.openiam.connector.type.constant.StatusCodeType;
 import org.openiam.connector.type.request.PasswordRequest;
 import org.openiam.connector.type.response.ResponseType;
-import org.openiam.connector.jdbc.command.base.AbstractAppTableCommand;
-import org.openiam.connector.jdbc.command.data.AppTableConfiguration;
 import org.springframework.stereotype.Service;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 @Service("setPasswordAppTableCommand")
 public class SetPasswordAppTableCommand extends AbstractAppTableCommand<PasswordRequest, ResponseType> {
@@ -25,13 +25,14 @@ public class SetPasswordAppTableCommand extends AbstractAppTableCommand<Password
 
         PreparedStatement statement = null;
         try {
-            statement = createSetPasswordStatement(con, configuration.getResourceId(), configuration.getTableName(), passwordRequest.getObjectIdentity(), passwordRequest.getPassword());
+            statement = createSetPasswordStatement(con, configuration.getResourceId(), configuration.getTableName(),
+                    passwordRequest.getObjectIdentity(), passwordRequest.getPassword());
             statement.executeUpdate();
             return response;
         } catch (SQLException se) {
             log.error(se.getMessage(), se);
             throw new ConnectorDataException(ErrorCode.CONNECTOR_ERROR, se.getMessage());
-        } catch(Throwable e) {
+        } catch (Throwable e) {
             log.error(e.getMessage(), e);
             throw new ConnectorDataException(ErrorCode.CONNECTOR_ERROR, e.getMessage());
         } finally {
