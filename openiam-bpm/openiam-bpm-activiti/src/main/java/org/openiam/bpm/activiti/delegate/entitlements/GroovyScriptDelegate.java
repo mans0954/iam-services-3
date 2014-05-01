e09a4148f828575791d2253421b538c1177e8280
Lev Bornovalov <lev.bornovalov@openiam.com>
2014-05-02 00:48:44 +0200

    Groovy script runner for Activiti

---
+++ openiam-bpm/openiam-bpm-activiti/src/main/java/org/openiam/bpm/activiti/delegate/entitlements/GroovyScriptDelegate.java
@@ -0,0 +1,41 @@
+package org.openiam.bpm.activiti.delegate.entitlements;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.impl.el.FixedValue;
+import org.apache.commons.lang.StringUtils;
+import org.openiam.bpm.activiti.delegate.core.AbstractActivitiJob;
+import org.openiam.script.ScriptIntegration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public class GroovyScriptDelegate extends AbstractActivitiJob {
+
+    @Autowired
+    @Qualifier("configurableGroovyScriptEngine")
+    private ScriptIntegration scriptRunner;
+	
+	private FixedValue scriptSrc;
+	
+	public GroovyScriptDelegate() {
+		super();
+	}
+	
+	@Override
+	public void execute(DelegateExecution execution) throws Exception {
+		if(scriptSrc == null) {
+			throw new IllegalArgumentException("scriptSrc is required");
+		}
+		
+		if(StringUtils.isBlank(scriptSrc.getExpressionText())) {
+			throw new IllegalArgumentException("scriptSrc is required");
+		}
+		
+		final String script = StringUtils.trimToNull(scriptSrc.getExpressionText());
+		
+		final Map<String, Object> bindingMap = new HashMap<>();
+		final Object retVal = scriptRunner.execute(bindingMap, script);
+	}
+}
