1355787772dc88014875a153fbe79c93e701d070
zhuk <alexander.zhuk@openiam.com>
2014-05-03 19:07:57 +0300

    [IDMAPPS-1690] Adding default Mng Sys attributes

--- openiam-idm/idm-connector-core/src/main/java/org/openiam/connector/common/command/LookupAttributeNamesCommand.java
+++ openiam-idm/idm-connector-core/src/main/java/org/openiam/connector/common/command/LookupAttributeNamesCommand.java
@@ -10,6 +10,7 @@ import org.openiam.connector.type.request.LookupRequest;
 import org.openiam.connector.type.response.LookupAttributeResponse;
 import org.openiam.exception.ScriptEngineException;
 import org.openiam.idm.srvc.mngsys.domain.ManagedSysEntity;
+import org.openiam.idm.srvc.mngsys.service.AttributeNamesLookupService;
 import org.openiam.provision.type.ExtensibleAttribute;
 import org.openiam.provision.type.ExtensibleObject;
 import org.openiam.script.ScriptIntegration;
@@ -35,36 +36,45 @@ public class LookupAttributeNamesCommand<ExtObject extends ExtensibleObject> ext
         LookupAttributeResponse respType = new LookupAttributeResponse();
         ConnectorConfiguration config = getConfiguration(lookupRequest.getTargetID(), ConnectorConfiguration.class);
         ManagedSysEntity mngSys = config.getManagedSys();
-        try {
 
-            Object attrNames = null;
-            if (StringUtils.isNotBlank(mngSys.getAttributeNamesLookup())) {
+        Object attrNames = null;
+        if (StringUtils.isNotBlank(mngSys.getAttributeNamesLookup())) {
+            try {
                 Map<String, Object> bindingMap = new HashMap<String, Object>();
                 bindingMap.put("managedSys", mngSys);
-                attrNames = scriptRunner.execute(bindingMap, mngSys.getAttributeNamesLookup());
-            }
-
-            List<ExtensibleAttribute> attributes = new ArrayList<ExtensibleAttribute>();
-            if (attrNames instanceof List) {
-                List<String> attrNamesList = (List<String>)attrNames;
-                for (String name : attrNamesList) {
-                    attributes.add(new ExtensibleAttribute(name, ""));
+                AttributeNamesLookupService lookupScript =
+                        (AttributeNamesLookupService) scriptRunner.instantiateClass(bindingMap,
+                                mngSys.getAttributeNamesLookup());
+                switch (lookupRequest.getExecutionMode()) {
+                    case "POLICY_MAP":
+                        attrNames = lookupScript.lookupPolicyMapAttributes(bindingMap);
+                        break;
+                    case "MANAGED_SYSTEM":
+                        attrNames = lookupScript.lookupManagedSystemAttributes(bindingMap);
+                        break;
                 }
-            } else if (attrNames instanceof Map) {
-                Map<String, String> attrNamesMap = (Map<String,String>)attrNames;
-
-                for (String name : attrNamesMap.keySet()) {
-                    attributes.add(new ExtensibleAttribute(name, "", attrNamesMap.get(name)));
-                }
+            } catch (Exception e) {
+                log.error("Can't execute script", e);
+                respType.setStatus(StatusCodeType.FAILURE);
             }
-
-            respType.setStatus(StatusCodeType.SUCCESS);
-            respType.setAttributes(attributes);
-
-        } catch (ScriptEngineException e) {
-            log.error("Can't execute script", e);
-            respType.setStatus(StatusCodeType.FAILURE);
         }
+
+        List<ExtensibleAttribute> attributes = new ArrayList<ExtensibleAttribute>();
+        if (attrNames instanceof List) {
+            List<String> attrNamesList = (List<String>)attrNames;
+            for (String name : attrNamesList) {
+                attributes.add(new ExtensibleAttribute(name, ""));
+            }
+        } else if (attrNames instanceof Map) {
+            Map<String, String> attrNamesMap = (Map<String,String>)attrNames;
+
+            for (String name : attrNamesMap.keySet()) {
+                attributes.add(new ExtensibleAttribute(name, "", attrNamesMap.get(name)));
+            }
+        }
+
+        respType.setStatus(StatusCodeType.SUCCESS);
+        respType.setAttributes(attributes);
 
         return respType;
     }
