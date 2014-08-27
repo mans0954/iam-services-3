b2cf78d92887d5115df698245461cc5b0e0db868
Lev Bornovalov <lev.bornovalov@openiam.com>
2014-08-27 02:05:26 +0200

    IDMAPPS-2069 Send Emails for tasks that are not claimed and/or completed. Batch Task

---
+++ openiam-bpm/openiam-bpm-activiti/src/main/java/org/openiam/bpm/activiti/notifier/TaskEmailNotifier.java
@@ -0,0 +1,78 @@
+package org.openiam.bpm.activiti.notifier;
+
+import java.util.List;
+
+import org.activiti.engine.TaskService;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.impl.persistence.entity.TaskEntity;
+import org.activiti.engine.task.IdentityLink;
+import org.activiti.engine.task.Task;
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.log4j.Logger;
+import org.openiam.idm.srvc.msg.dto.NotificationParam;
+import org.openiam.idm.srvc.msg.dto.NotificationRequest;
+import org.openiam.idm.srvc.msg.service.MailService;
+import org.openiam.idm.srvc.user.domain.UserEntity;
+import org.openiam.idm.srvc.user.service.UserDAO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component("taskEmailNotifier")
+public class TaskEmailNotifier {
+	
+	private static Logger LOG = Logger.getLogger(TaskEmailNotifier.class);
+	
+	@Autowired
+	@Qualifier("activitiTaskService")
+	private TaskService taskService;
+	
+	@Autowired
+	private UserDAO userDAO;
+	
+	@Autowired
+	protected MailService mailService;
+
+	@Transactional
+	public void sweep() {
+		
+		List<Task> taskList = null;
+		int firstResult = 0;
+		int maxResults = 1000;
+		
+		taskList = taskService.createTaskQuery().listPage(firstResult, maxResults);
+		sendAssigneeReminders(taskList);
+		while(CollectionUtils.isNotEmpty(taskList)) {
+			firstResult += maxResults;
+			taskList = taskService.createTaskQuery().listPage(firstResult, maxResults);
+			sendAssigneeReminders(taskList);
+		}
+	}
+	
+	private void sendAssigneeReminders(final List<Task> taskList) {
+		if(CollectionUtils.isNotEmpty(taskList)) {
+			for(final Task task : taskList) {
+				final TaskEntity entity = (TaskEntity)task;
+				if(StringUtils.isNotBlank(entity.getAssignee())) {
+					LOG.info(entity.getAssignee());
+					sendAssigneeReminder(entity.getAssignee(), entity);
+				}
+			}
+		}
+	}
+	
+	private void sendAssigneeReminder(final String userId, final TaskEntity task) {
+		final UserEntity user = userDAO.findById(userId);
+		if(user != null) {
+			final NotificationRequest request = new NotificationRequest();
+	        request.setUserId(user.getId());
+	        request.setNotificationType("TASK_ASSIGNEE_REMINDER");
+	        request.getParamList().add(new NotificationParam("TASK_NAME", task.getName()));
+	        request.getParamList().add(new NotificationParam("TASK_DESCRIPTION", task.getDescription()));
+	        request.getParamList().add(new NotificationParam("TARGET_USER", user.getDisplayName()));
+	        mailService.sendNotification(request);
+		}
+	}
+}
