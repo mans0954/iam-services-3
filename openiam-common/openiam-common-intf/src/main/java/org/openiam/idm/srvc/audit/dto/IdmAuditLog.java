bbedb84aba1c7d1c7fa2c99c00330864fcc57e14
Vitaly Yakunin <vitaly.yakunin@gmail.com>
2014-09-22 17:21:15 +0300

    [IDMAPPS-2122] Child Event Order incorrect

--- openiam-common/openiam-common-intf/src/main/java/org/openiam/idm/srvc/audit/dto/IdmAuditLog.java
+++ openiam-common/openiam-common-intf/src/main/java/org/openiam/idm/srvc/audit/dto/IdmAuditLog.java
@@ -3,9 +3,7 @@ package org.openiam.idm.srvc.audit.dto;
 // Generated Nov 30, 2007 3:01:45 AM by Hibernate Tools 3.2.0.b11
 
 import java.io.Serializable;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
@@ -189,6 +187,23 @@ public class IdmAuditLog implements Serializable {
 		return childLogs;
 	}
 
+    /**
+     * Sorting by timestamp - DESC
+     * @return
+     */
+    public Collection<IdmAuditLog> getChildLogsSorted() {
+        if(childLogs != null) {
+            List<IdmAuditLog> sortedItems = new ArrayList<IdmAuditLog>(childLogs);
+            Collections.sort(sortedItems, new Comparator<IdmAuditLog>(){
+                @Override
+                public int compare(IdmAuditLog o1, IdmAuditLog o2) {
+                    return o2.getTimestamp().compareTo(o1.getTimestamp());
+                }
+            } );
+            return sortedItems;
+        }
+        return childLogs;
+    }
 	public void setChildLogs(Set<IdmAuditLog> childLogs) {
 		this.childLogs = childLogs;
 	}
