e050ebdce3cfde35373b4c4be13cfa2c8d45f585
Vitaly Yakunin <vitaly.yakunin@gmail.com>
2014-10-13 22:32:20 +0300

    fix equals/hash IdmAuditLogEntity

--- openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/audit/service/AuditLogServiceImpl.java
+++ openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/audit/service/AuditLogServiceImpl.java
@@ -84,14 +84,6 @@ public class AuditLogServiceImpl implements AuditLogService {
 
     @Autowired
     private IdmAuditLogCustomDozerConverter idmAuditLogCustomDozerConverter;
-    /**
-     * Cache for UserId and CorrelationId
-     *
-     * When new User login: new AuditLog with Correlation Id will be generated and store in cache Map
-     * Every login we replace CorrelationId by UserId
-     *
-     */
-    private final Map<String, String> correlationIdByRequesterId = new HashMap<>();
 
     @PostConstruct
     public void init() {
@@ -110,21 +102,12 @@ public class AuditLogServiceImpl implements AuditLogService {
                 auditLogEntity.setHash(DigestUtils.sha256Hex(log.concat()));
             }
 
-            if(StringUtils.isEmpty(auditLogEntity.getCorrelationId())) {
-               // log.setCorrelationId(String.valueOf(new Random().nextLong()));
-            }
             auditLogEntity.setNodeIP(nodeIP);
 
     		if(CollectionUtils.isNotEmpty(log.getChildLogs())) {
     			for(final IdmAuditLog ch : log.getChildLogs()) {
-                    if(StringUtils.isEmpty(ch.getCorrelationId())) {
-                       // log.setCorrelationId(log.getCorrelationId());
-                    }
                     IdmAuditLogEntity chEntity = prepare(ch);
-                    if(!logExists(auditLogEntity.getChildLogs(), chEntity)) {
-
-                      //  auditLogEntity.getChildLogs().contains(chEntity)
-
+                    if(!auditLogEntity.getChildLogs().contains(chEntity)) {
                         auditLogEntity.addChild(chEntity);
                         chEntity.addParent(auditLogEntity);
                     }
@@ -174,16 +157,6 @@ public class AuditLogServiceImpl implements AuditLogService {
             return auditLogEntity;
         }
         return null;
-    }
-
-    private boolean logExists(Set<IdmAuditLogEntity> logEntitySet, IdmAuditLogEntity logEntity) {
-        if(CollectionUtils.isNotEmpty(logEntitySet)){
-            for(IdmAuditLogEntity log : logEntitySet){
-                if(log!=null && log.equals(logEntity))
-                    return true;
-            }
-        }
-        return false;
     }
 
     @Override
