88a2e4d2bc2d61071a3dcfdb93685169a2536040
Lev Bornovalov <lev.bornovalov@openiam.com>
2014-11-16 02:54:11 +0100

    AM-402
    Add option that specifies "Do not send cookie with Name XYZ", and send
    that as a flag to the proxy

---
+++ openiam-am/openiam-am-services/src/main/java/org/openiam/am/srvc/uriauth/rule/ExcludeCookieURIPatternRule.java
@@ -0,0 +1,8 @@
+package org.openiam.am.srvc.uriauth.rule;
+
+import org.springframework.stereotype.Component;
+
+@Component("excludeCookieRule")
+public class ExcludeCookieURIPatternRule extends AbstractURIPatternRule {
+
+}
---
+++ openiam-am/openiam-am-services/src/main/java/org/openiam/am/srvc/uriauth/rule/ExcludeHeaderURIPatternRule.java
@@ -0,0 +1,8 @@
+package org.openiam.am.srvc.uriauth.rule;
+
+import org.springframework.stereotype.Component;
+
+@Component("excludeHeaderRule")
+public class ExcludeHeaderURIPatternRule extends AbstractURIPatternRule {
+
+}
