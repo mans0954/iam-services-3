fe1433ec20ca036457fe05789158a42934cbace5
Sergey Onuchin <serg.onuchin@gmail.com>
2014-11-17 23:19:50 +0300

    [IDMAPPS-2366] remove reconciliation transaction

--- openiam-idm/openiam-idm-services/src/main/java/org/openiam/idm/srvc/recon/command/DeleteResourceAccountUserCommand.java
+++ openiam-idm/openiam-idm-services/src/main/java/org/openiam/idm/srvc/recon/command/DeleteResourceAccountUserCommand.java
@@ -1,39 +1,29 @@
 package org.openiam.idm.srvc.recon.command;
 
-import org.apache.commons.collections.CollectionUtils;
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openiam.base.AttributeOperationEnum;
-import org.openiam.base.BaseAttribute;
 import org.openiam.connector.type.request.CrudRequest;
 import org.openiam.idm.srvc.auth.dto.Login;
 import org.openiam.idm.srvc.mngsys.dto.ManagedSysDto;
 import org.openiam.idm.srvc.mngsys.ws.ManagedSystemWebService;
 import org.openiam.idm.srvc.recon.dto.ReconciliationSituation;
-import org.openiam.idm.srvc.recon.service.PopulationScript;
-import org.openiam.idm.srvc.recon.service.ReconciliationObjectCommand;
 import org.openiam.idm.srvc.user.dto.User;
 import org.openiam.provision.dto.ProvisionUser;
 import org.openiam.provision.resp.ProvisionUserResponse;
-import org.openiam.provision.service.AbstractProvisioningService;
 import org.openiam.provision.service.ConnectorAdapter;
 import org.openiam.provision.service.ProvisionService;
 import org.openiam.provision.type.ExtensibleAttribute;
 import org.openiam.provision.type.ExtensibleUser;
-import org.openiam.script.ScriptIntegration;
 import org.openiam.util.MuleContextProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Component("deleteResourceAccountUserCommand")
-public class DeleteResourceAccountUserCommand implements ReconciliationObjectCommand<User> {
+public class DeleteResourceAccountUserCommand extends BaseReconciliationUserCommand {
 
     private static final Log log = LogFactory.getLog(DeleteResourceAccountUserCommand.class);
 
@@ -47,20 +37,17 @@ public class DeleteResourceAccountUserCommand implements ReconciliationObjectCom
     @Qualifier("defaultProvision")
     private ProvisionService provisionService;
 
-    @Autowired
-    @Qualifier("configurableGroovyScriptEngine")
-    private ScriptIntegration scriptRunner;
-
     public DeleteResourceAccountUserCommand(){
     }
 
-    @Override
-    public boolean execute(ReconciliationSituation config, String principal, String mSysID, User user, List<ExtensibleAttribute> attributes) {
+	@Override
+	public boolean execute(ReconciliationSituation config, String principal, String mSysID, User user, List<ExtensibleAttribute> attributes) {
         log.debug("Entering DeleteResourceAccountCommand");
-        if(user == null) {
+		log.debug("Delete Resource for principal: " + principal);
+
+		if(user == null) {
             ManagedSysDto mSys = managedSysService.getManagedSys(mSysID);
 
-            log.debug("Calling delete with Remote connector");
             CrudRequest<ExtensibleUser> request = new CrudRequest<ExtensibleUser>();
             request.setObjectIdentity(principal);
             request.setTargetID(mSysID);
@@ -68,7 +55,6 @@ public class DeleteResourceAccountUserCommand implements ReconciliationObjectCom
             request.setHostLoginPassword(mSys.getDecryptPassword());
             request.setHostUrl(mSys.getHostUrl());
             request.setScriptHandler(mSys.getDeleteHandler());
-            log.debug("Calling delete local connector");
             connectorAdapter.deleteRequest(mSys, request, MuleContextProvider.getCtx());
 
             return true;
@@ -81,44 +67,18 @@ public class DeleteResourceAccountUserCommand implements ReconciliationObjectCom
             }
         }
 
-        ProvisionUser pUser = new ProvisionUser(user);
-        pUser.setPrincipalList(principleList);
-        pUser.setSrcSystemId(mSysID);
-        if(StringUtils.isNotEmpty(config.getScript())){
-            try {
-                Map<String, String> line = new HashMap<String, String>();
-                for (ExtensibleAttribute attr : attributes) {
-                    if (attr.getValue() != null) {
-                        line.put(attr.getName(), attr.getValue());
-                    } else if (attr.getAttributeContainer() != null &&
-                            CollectionUtils.isNotEmpty(attr.getAttributeContainer().getAttributeList()) &&
-                            line.get(attr.getName()) == null) {
-                        StringBuilder value = new StringBuilder();
-                        boolean isFirst = true;
-                        for (BaseAttribute ba : attr.getAttributeContainer().getAttributeList()) {
-                            if (!isFirst) {
-                                value.append('^');
-                            } else {
-                                isFirst = false;
-                            }
-                            value.append(ba.getValue());
-                        }
-                        line.put(attr.getName(), value.toString());
-                    }
-                }
-                Map<String, Object> bindingMap = new HashMap<String, Object>();
-                bindingMap.put(AbstractProvisioningService.TARGET_SYS_MANAGED_SYS_ID, mSysID);
-                PopulationScript script = (PopulationScript) scriptRunner.instantiateClass(bindingMap, config.getScript());
-                int retval = script.execute(line, pUser);
-                //Reset source system flag from User to avoid ignoring Provisioning for this resource
-                pUser.setSrcSystemId(null);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-        ProvisionUserResponse response = provisionService.modifyUser(pUser);
-        return response.isSuccess();
+		try {
+			ProvisionUser pUser = new ProvisionUser(user);
+			pUser.setPrincipalList(principleList);
+			pUser.setSrcSystemId(mSysID);
+			executeScript(config.getScript(), attributes, pUser);
+			//Reset source system flag from User to avoid ignoring Provisioning for this resource
+			pUser.setSrcSystemId(null);
+			ProvisionUserResponse response = provisionService.modifyUser(pUser);
+			return response.isSuccess();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return false;
+		}
     }
 }
---
+++ openiam-idm/openiam-idm-services/src/main/java/org/openiam/idm/srvc/recon/service/ReconciliationConfigServiceImpl.java
@@ -0,0 +1,272 @@
+/*
+ * Copyright 2009, OpenIAM LLC 
+ * This file is part of the OpenIAM Identity and Access Management Suite
+ *
+ *   OpenIAM Identity and Access Management Suite is free software: 
+ *   you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License 
+ *   version 3 as published by the Free Software Foundation.
+ *
+ *   OpenIAM is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   Lesser GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with OpenIAM.  If not, see <http://www.gnu.org/licenses/>. *
+ */
+
+/**
+ *
+ */
+package org.openiam.idm.srvc.recon.service;
+
+import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openiam.base.SysConfiguration;
+import org.openiam.base.ws.ResponseCode;
+import org.openiam.base.ws.ResponseStatus;
+import org.openiam.dozer.converter.GroupDozerConverter;
+import org.openiam.dozer.converter.ManagedSystemObjectMatchDozerConverter;
+import org.openiam.dozer.converter.ReconciliationConfigDozerConverter;
+import org.openiam.dozer.converter.ReconciliationSituationDozerConverter;
+import org.openiam.idm.parser.csv.UserCSVParser;
+import org.openiam.idm.parser.csv.UserSearchBeanCSVParser;
+import org.openiam.idm.searchbeans.ManualReconciliationSearchBean;
+import org.openiam.idm.searchbeans.ReconConfigSearchBean;
+import org.openiam.idm.srvc.audit.constant.AuditAction;
+import org.openiam.idm.srvc.audit.constant.AuditAttributeName;
+import org.openiam.idm.srvc.audit.dto.IdmAuditLog;
+import org.openiam.idm.srvc.audit.service.AuditLogService;
+import org.openiam.idm.srvc.auth.login.LoginDataService;
+import org.openiam.idm.srvc.grp.service.GroupDataService;
+import org.openiam.idm.srvc.mngsys.domain.ManagedSysEntity;
+import org.openiam.idm.srvc.mngsys.service.ManagedSystemService;
+import org.openiam.idm.srvc.recon.domain.ReconciliationConfigEntity;
+import org.openiam.idm.srvc.recon.domain.ReconciliationSituationEntity;
+import org.openiam.idm.srvc.recon.dto.ReconExecStatusOptions;
+import org.openiam.idm.srvc.recon.dto.ReconciliationConfig;
+import org.openiam.idm.srvc.recon.dto.ReconciliationResponse;
+import org.openiam.idm.srvc.recon.dto.ReconciliationSituation;
+import org.openiam.idm.srvc.recon.result.dto.ReconciliationResultBean;
+import org.openiam.idm.srvc.recon.result.dto.ReconciliationResultField;
+import org.openiam.idm.srvc.recon.result.dto.ReconciliationResultRow;
+import org.openiam.idm.srvc.recon.result.dto.ReconcliationFieldComparatorByField;
+import org.openiam.idm.srvc.recon.util.Serializer;
+import org.openiam.idm.srvc.res.service.ResourceDataService;
+import org.openiam.idm.srvc.role.service.RoleDataService;
+import org.openiam.idm.srvc.synch.srcadapter.MatchRuleFactory;
+import org.openiam.provision.service.ConnectorAdapter;
+import org.openiam.provision.service.PrePostExecutor;
+import org.openiam.script.ScriptIntegration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.ApplicationContext;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+
+/**
+ * @author suneet
+ * 
+ */
+@Service
+public class ReconciliationConfigServiceImpl implements ReconciliationConfigService {
+
+    @Autowired
+    protected ReconciliationSituationDAO reconSituationDAO;
+
+    @Autowired
+    protected ReconciliationConfigDAO reconConfigDao;
+
+    @Autowired
+    protected LoginDataService loginManager;
+    @Autowired
+    protected ResourceDataService resourceDataService;
+    @Autowired
+    protected ManagedSystemObjectMatchDozerConverter objectMatchDozerConverter;
+    @Autowired
+    protected ManagedSystemService managedSysService;
+    @Autowired
+    protected ConnectorAdapter connectorAdapter;
+    @Autowired
+    protected GroupDataService groupManager;
+    @Autowired
+    protected GroupDozerConverter groupDozerConverter;
+    @Autowired
+    protected RoleDataService roleDataService;
+    @Autowired
+    protected SysConfiguration sysConfiguration;
+    @Autowired
+    private ReconciliationConfigDozerConverter reconConfigDozerMapper;
+    @Autowired
+    private ReconciliationSituationDozerConverter reconSituationDozerMapper;
+    @Autowired
+    protected UserCSVParser userCSVParser;
+    @Autowired
+    public UserSearchBeanCSVParser userSearchCSVParser;
+    @Autowired
+    @Qualifier("configurableGroovyScriptEngine")
+    protected ScriptIntegration scriptRunner;
+    @Value("${iam.files.location}")
+    private String absolutePath;
+
+    @Autowired
+    protected MatchRuleFactory matchRuleFactory;
+    @Autowired
+    protected AuditLogService auditLogService;
+
+    @Transactional
+    public ReconciliationConfig addConfig(ReconciliationConfig config) {
+        if (config == null) {
+            throw new IllegalArgumentException("config parameter is null");
+        }
+        Set<ReconciliationSituation> sitSet = null;
+        if (!CollectionUtils.isEmpty(config.getSituationSet())) {
+            sitSet = new HashSet<>(config.getSituationSet());
+        }
+        config.setReconConfigId(null);
+        ReconciliationConfig result = reconConfigDozerMapper.convertToDTO(
+                reconConfigDao.add(reconConfigDozerMapper.convertToEntity(config, false)), false);
+        saveSituationSet(sitSet, result.getReconConfigId());
+        result.setSituationSet(sitSet);
+        return result;
+    }
+
+    @Transactional
+    private void saveSituationSet(Set<ReconciliationSituation> sitSet, String configId) {
+        if (sitSet != null) {
+            for (ReconciliationSituation s : sitSet) {
+                if (StringUtils.isEmpty(s.getReconConfigId())) {
+                    s.setReconConfigId(configId);
+                }
+                if (StringUtils.isEmpty(s.getReconSituationId())) {
+                    s.setReconSituationId(null);
+                    s.setReconSituationId(reconSituationDAO.add(reconSituationDozerMapper.convertToEntity(s, false))
+                            .getReconSituationId());
+                } else {
+                    reconSituationDAO.update(reconSituationDozerMapper.convertToEntity(s, false));
+                }
+            }
+        }
+    }
+
+    @Transactional
+    public void updateConfig(ReconciliationConfig config) {
+        if (config == null) {
+            throw new IllegalArgumentException("config parameter is null");
+        }
+
+        ReconciliationConfigEntity configEntity = reconConfigDozerMapper.convertToEntity(config, false);
+
+        for (ReconciliationSituation s : config.getSituationSet()) {
+            if (StringUtils.isEmpty(s.getReconConfigId())) {
+                s.setReconConfigId(configEntity.getReconConfigId());
+            }
+            ReconciliationSituationEntity situationEntity;
+            if (StringUtils.isEmpty(s.getReconSituationId())) {
+                situationEntity = reconSituationDozerMapper.convertToEntity(s, false);
+                reconSituationDAO.save(situationEntity);
+            } else {
+                situationEntity = reconSituationDAO.findById(s.getReconSituationId());
+                situationEntity.setScript(s.getScript());
+                situationEntity.setSituation(s.getSituation());
+                situationEntity.setSituationResp(s.getSituationResp());
+                situationEntity.setCustomCommandScript(s.getCustomCommandScript());
+                reconSituationDAO.save(situationEntity);
+            }
+            configEntity.getSituationSet().add(situationEntity);
+        }
+
+        reconConfigDao.merge(configEntity);
+
+    }
+
+    @Transactional
+    public void removeConfig(String configId) {
+        if (configId == null) {
+            throw new IllegalArgumentException("configId parameter is null");
+        }
+        ReconciliationConfigEntity config = reconConfigDao.findById(configId);
+        reconConfigDao.delete(config);
+
+    }
+
+    @Transactional(readOnly = true)
+    public ReconciliationConfig getConfigByResourceByType(String resourceId, String type) {
+        if (resourceId == null) {
+            throw new IllegalArgumentException("resourceId parameter is null");
+        }
+        ReconciliationConfigEntity result = reconConfigDao.findByResourceIdByType(resourceId, type);
+        if (result == null)
+            return null;
+        else
+            return reconConfigDozerMapper.convertToDTO(result, true);
+
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ReconciliationConfig> findReconConfig(ReconConfigSearchBean searchBean, int from, int size) {
+        List<ReconciliationConfigEntity> reconciliationConfigEntities = reconConfigDao.getByExample(searchBean, from, size);
+        if(reconciliationConfigEntities == null) {
+            return Collections.EMPTY_LIST;
+        }
+        return reconConfigDozerMapper.convertToDTOList(reconciliationConfigEntities, false);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public int countReconConfig(final ReconConfigSearchBean searchBean) {
+        return reconConfigDao.count(searchBean);
+    }
+
+    @Transactional(readOnly = true)
+    public List<ReconciliationConfig> getConfigsByResource(final String resourceId) {
+        if (resourceId == null) {
+            throw new IllegalArgumentException("resourceId parameter is null");
+        }
+        List<ReconciliationConfigEntity> result = reconConfigDao.findByResourceId(resourceId);
+        if (result == null)
+            return new LinkedList<ReconciliationConfig>();
+        else
+            return reconConfigDozerMapper.convertToDTOList(result, false);
+    }
+
+    @Transactional(readOnly = true)
+    public ReconciliationConfig getConfigById(String configId) {
+        if (configId == null) {
+            throw new IllegalArgumentException("configId parameter is null");
+        }
+        ReconciliationConfigEntity result = reconConfigDao.findById(configId);
+        if (result == null)
+            return null;
+        else
+            return reconConfigDozerMapper.convertToDTO(result, true);
+    }
+
+	@Transactional(readOnly = true)
+	public ReconExecStatusOptions getExecStatus(String configId) {
+		ReconciliationConfigEntity config = reconConfigDao.findById(configId);
+		if (config != null) {
+			reconConfigDao.refresh(config);
+			return config.getExecStatus();
+		} else {
+			return null;
+		}
+	}
+
+	@Transactional
+	public void updateExecStatus(String configId, ReconExecStatusOptions status) {
+		ReconciliationConfigEntity config = reconConfigDao.findById(configId);
+		config.setExecStatus(status);
+		reconConfigDao.update(config);
+	}
+}
