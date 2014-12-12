862f048d8fdbeb2528ae897ceb9c3c96915240a3
Lev Bornovalov <lev.bornovalov@openiam.com>
2014-12-12 04:27:52 +0100

    3) IDMAPPS-2486 SelfReg requests throw NPE due to back Jackson Mapper

--- openiam-common/openiam-common-intf/src/main/java/org/openiam/idm/util/CustomJacksonMapper.java
+++ openiam-common/openiam-common-intf/src/main/java/org/openiam/idm/util/CustomJacksonMapper.java
@@ -1,9 +1,19 @@
 package org.openiam.idm.util;
 
+import java.io.IOException;
+
+import org.apache.commons.lang.StringUtils;
+import org.springframework.web.util.HtmlUtils;
+
 import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.Version;
+import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonDeserializer;
 import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 
 
 public class CustomJacksonMapper extends ObjectMapper {
@@ -14,6 +24,17 @@ public class CustomJacksonMapper extends ObjectMapper {
 		 .configure(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT, true)
 		 .configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false)
 		 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		final SimpleModule stringModule = new SimpleModule("MyModule", new Version(1, 0, 0, null)).addDeserializer(String.class, new StringDeserializer());
+		this.registerModule(stringModule);
+	}
+	
+	private class StringDeserializer extends JsonDeserializer<String> {
+        @Override
+        public String deserialize(JsonParser parser, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            String str = StringUtils.trimToNull(parser.getText());
+            return str;
+        }
+
 	}
 	
 	public String mapToStringQuietly(final Object o) {
