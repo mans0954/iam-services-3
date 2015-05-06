31fd52b2a777db5329282c69aeb0eaf1f727c80e
Sergey Onuchin <serg.onuchin@gmail.com>
2015-05-06 21:04:41 +0300

    Fix charset for NON_ALPHA_CHARS rule

--- openiam-common/openiam-common-intf/src/main/java/org/openiam/idm/srvc/pswd/service/PasswordGenerator.java
+++ openiam-common/openiam-common-intf/src/main/java/org/openiam/idm/srvc/pswd/service/PasswordGenerator.java
@@ -41,9 +41,10 @@ public class PasswordGenerator {
     private static final char[] lowerChars = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
     private static final char[] upperChars = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
     private static final char[] numericChars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-    private static final char[] specialChars = {'!', '$', '@', '%', '&', '{', '}', '*', '#', '%', '+', '-', '_', '/', '?'};
+    private static final char[] specialChars = {'!', '\"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/',
+                                                ':', ';', '<', '=', '>', '?', '@', '[', '\\', ']', '^', '_', '`', '{', '|', '}', '~'};
 
-    private static final String charset = "!$@%&{}*#%+-_/?0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private static final String charset = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
     private static final String specialCharset = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
     private static final String numericCharset = "0123456789";
