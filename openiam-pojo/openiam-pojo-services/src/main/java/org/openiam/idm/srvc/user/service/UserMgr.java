48e93af7ed46d01467b09016920a37e9d7f6f915
Anton Kovtun <anton.kovtun93@gmail.com>
2015-08-05 01:18:49 +0300

    [IDMAPPS-3210] Principal field is blank in search superior/subordinate

--- openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/user/service/UserMgr.java
+++ openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/user/service/UserMgr.java
@@ -1569,7 +1569,15 @@ public class UserMgr implements UserDataService {
             userIds = getUserIds(searchBean);
         }
         userIds.removeAll(userDao.getAllAttachedSupSubIds(searchBean.getTargetUserIds()));
-        return userDao.findByIds(userIds);
+
+        List<UserEntity> entityList = userDao.findByIds(userIds);
+
+        if(CollectionUtils.isNotEmpty(entityList)
+                && searchBean.getInitDefaulLoginFlag()){
+            setDefaultLogin(entityList);
+        }
+
+        return entityList;
     }
 
     @Override
