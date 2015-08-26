1f18467245aab4967640cea27d8f05495e051805
Lev Bornovalov <lbornov2@gmail.com>
2015-08-26 16:52:49 +0200

    Fix Vitaly's commit

--- openiam-idm/openiam-idm-services/src/main/java/org/openiam/idm/srvc/synch/rs/SynchRestEndpoint.java
+++ openiam-idm/openiam-idm-services/src/main/java/org/openiam/idm/srvc/synch/rs/SynchRestEndpoint.java
@@ -1,24 +1,27 @@
 package org.openiam.idm.srvc.synch.rs;
 
-import org.apache.commons.lang.StringUtils;
-
-import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+
+import org.apache.commons.lang.StringUtils;
 import org.openiam.idm.srvc.synch.ws.AsynchIdentitySynchService;
 import org.openiam.idm.srvc.synch.ws.IdentitySynchWebService;
 import org.openiam.idm.srvc.synch.ws.SynchConfigResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import javax.ws.rs.*;
-import javax.ws.rs.PathParam;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by Vitaly on 8/24/2015.
  */
-@Path("/synch")
-@Scope("prototype")
-@Component("synchRestService")
+@RestController("/synch")
+//@Path("/synch")
+//@Scope("prototype")
+//@Component("synchRestService")
 public class SynchRestEndpoint {
     @Autowired
     private IdentitySynchWebService synchConfigServiceClient;
@@ -26,11 +29,9 @@ public class SynchRestEndpoint {
     @Autowired
     private AsynchIdentitySynchService asynchIdentitySynchService;
 
-    @POST
-    @Path("/run/{synchId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response execute(final @PathParam(value="synchId") String synchId,
-                          final @FormParam("json") String jsonValue) {
+    @RequestMapping(value="/run/{synchId}", method=RequestMethod.POST)
+    public @ResponseBody Response execute(final @PathVariable(value="synchId") String synchId,
+                            final @RequestParam(value="json") String jsonValue) {
         Response ajaxResponse = Response.accepted().build();
         try {
             if (StringUtils.isNotBlank(synchId)) {
