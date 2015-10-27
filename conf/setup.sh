b62ecc1cf74481d6a0893e906e03e4a4f2e67a54
Lev Bornovalov <lbornov2@gmail.com>
2015-10-27 19:36:19 +0100

    more circle patches

--- conf/setup.sh
+++ conf/setup.sh
@@ -9,6 +9,7 @@ sudo mkdir -p /opt/openiam/webapps
 sudo cp client.rb /etc/chef/client.rb
 sudo cp client.pem /etc/chef/client.pem
 sudo cp attributes.json /etc/chef/attributes.json
+sudo chmod 777 /opt/openiam/webapps
 sudo chef-client -o openiamapp::java8 -E DEV
 sudo chef-client -o openiam-properties::datasource -E DEV -j /etc/chef/attributes.json
 sudo chef-client -o openiam-properties::securityconf -E DEV -j /etc/chef/attributes.json
