c574679acf16732188c1a71e2edcdc70b08a6502
Lev Bornovalov <lbornov2@gmail.com>
2015-10-27 18:07:14 +0100

    more setup updates

--- conf/setup.sh
+++ conf/setup.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 set -e
-set -x 
+set -x
 
-mkdir -p /data/openiam/conf
-mkdir -p /etc/chef
-mkdir -p /opt/openiam/webapps
-cp conf/client.rb /etc/chef/client.rb
-cp conf/client.pem /etc/chef/client.pem
-cp conf/attributes.json /etc/chef/attributes.json
-chef-client -o openiam-properties::datasource -E DEV
-chef-client -o openiam-properties::securityconf -E DEV
-chef-client -o openiam-properties::service-urls -E DEV
-chef-client -o openiam-conf -E DEV
-chef-client -o openiam-hazelcast -E DEV -j /etc/chef/attributes.json
+sudo mkdir -p /data/openiam/conf
+sudo mkdir -p /etc/chef
+sudo mkdir -p /opt/openiam/webapps
+sudo cp client.rb /etc/chef/client.rb
+sudo cp client.pem /etc/chef/client.pem
+sudo cp attributes.json /etc/chef/attributes.json
+sudo chef-client -o openiam-properties::datasource -E DEV
+sudo chef-client -o openiam-properties::securityconf -E DEV
+sudo chef-client -o openiam-properties::service-urls -E DEV
+sudo chef-client -o openiam-conf -E DEV
+sudo chef-client -o openiam-hazelcast -E DEV -j /etc/chef/attributes.json
+sudo apt-add-repository -y 'deb http://ppa.launchpad.net/ondrej/mysql-experimental/ubuntu precise main'
+sudo apt-get update; sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server-5.6
 mysql -u root --execute="SET PASSWORD FOR 'root'@'localhost' = PASSWORD('passwd00')"
