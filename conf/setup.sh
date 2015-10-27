117877d72ce745ed245ba50593f26a3c40d5c568
Lev Bornovalov <lbornov2@gmail.com>
2015-10-27 18:32:36 +0100

    now attempt to run mysql scripts

--- conf/setup.sh
+++ conf/setup.sh
@@ -9,12 +9,14 @@ sudo mkdir -p /opt/openiam/webapps
 sudo cp client.rb /etc/chef/client.rb
 sudo cp client.pem /etc/chef/client.pem
 sudo cp attributes.json /etc/chef/attributes.json
-mysql -u root --execute="SET PASSWORD FOR 'root'@'localhost' = PASSWORD('passwd00')"
-sudo apt-add-repository -y 'deb http://ppa.launchpad.net/ondrej/mysql-experimental/ubuntu precise main'
-sudo apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server-5.6
+# mysql -u root --execute="SET PASSWORD FOR 'root'@'localhost' = PASSWORD('passwd00')"
+# sudo apt-add-repository -y 'deb http://ppa.launchpad.net/ondrej/mysql-experimental/ubuntu precise main'
+# sudo apt-get update
+# sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server-5.6
 sudo chef-client -o openiam-properties::datasource -E DEV
 sudo chef-client -o openiam-properties::securityconf -E DEV
 sudo chef-client -o openiam-properties::service-urls -E DEV
 sudo chef-client -o openiam-conf -E DEV
 sudo chef-client -o openiam-hazelcast -E DEV -j /etc/chef/attributes.json
+cd /tmp/openiam/schema/mysql
+sudo ./execute.sh passwd00 new_install
