1f0db8c6aa405e04c553efc5200d09edda21c7cc
Lev Bornovalov <lbornov2@gmail.com>
2015-10-27 19:24:25 +0100

    add java 8 installation with jce keys

--- conf/setup.sh
+++ conf/setup.sh
@@ -9,10 +9,7 @@ sudo mkdir -p /opt/openiam/webapps
 sudo cp client.rb /etc/chef/client.rb
 sudo cp client.pem /etc/chef/client.pem
 sudo cp attributes.json /etc/chef/attributes.json
-# mysql -u root --execute="SET PASSWORD FOR 'root'@'localhost' = PASSWORD('passwd00')"
-# sudo apt-add-repository -y 'deb http://ppa.launchpad.net/ondrej/mysql-experimental/ubuntu precise main'
-# sudo apt-get update
-# sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server-5.6
+sudo chef-client -o openiamapp::java8 -E DEV
 sudo chef-client -o openiam-properties::datasource -E DEV -j /etc/chef/attributes.json
 sudo chef-client -o openiam-properties::securityconf -E DEV -j /etc/chef/attributes.json
 sudo chef-client -o openiam-properties::service-urls -E DEV -j /etc/chef/attributes.json
