14099db02f0f0bdeb7c1ea6b5f85f1e456796b07
Lev Bornovalov <lbornov2@gmail.com>
2015-11-02 23:55:39 +0100

    fix sh script

--- conf/deploy.sh
+++ conf/deploy.sh
@@ -5,6 +5,6 @@ set -x
 
 mvn deploy -s conf/settings.xml
 /bin/sh update_version.sh $(mvn help:evaluate -Dexpression=project.version | grep -v '\[.*')-${CIRCLE_BUILD_NUM}
-install -s conf/settings.xml
+mvn install -s conf/settings.xml
 mvn package spring-boot:repackage -f openiam-esb/pom.xml
 mvn deploy -s conf/settings.xml
