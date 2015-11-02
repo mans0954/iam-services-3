b97dc0479d4ecc1c03dbe6ed5adbb66921a56b67
Lev Bornovalov <lbornov2@gmail.com>
2015-11-02 23:29:31 +0100

    fix

--- conf/deploy.sh
+++ conf/deploy.sh
@@ -3,8 +3,8 @@
 set -e
 set -x
 
-mvn deploy -s settings.xml
-/bin/sh ../update_version.sh $(mvn help:evaluate -Dexpression=project.version | grep -v '\[.*')-${CIRCLE_BUILD_NUM}
-install -s settings.xml
-mvn package spring-boot:repackage -f ../openiam-esb/pom.xml
-mvn deploy settings.xml
+mvn deploy -s conf/settings.xml
+/bin/sh update_version.sh $(mvn help:evaluate -Dexpression=project.version | grep -v '\[.*')-${CIRCLE_BUILD_NUM}
+install -s conf/settings.xml
+mvn package spring-boot:repackage -f openiam-esb/pom.xml
+mvn deploy -s conf/settings.xml
