2566d0c0f15f36ba0c91229b31c3079a561a9680
Alexander Duckardt <aduckardt@gmail.com>
2015-11-06 23:41:08 -0500

    AM-763. Fix oAuth scopeValidation

--- openiam-common/openiam-common-intf/src/main/java/org/openiam/am/srvc/dto/OAuthScopesResponse.java
+++ openiam-common/openiam-common-intf/src/main/java/org/openiam/am/srvc/dto/OAuthScopesResponse.java
@@ -1,5 +1,8 @@
 package org.openiam.am.srvc.dto;
 
+import org.openiam.base.ws.Response;
+import org.openiam.base.ws.ResponseCode;
+import org.openiam.base.ws.ResponseStatus;
 import org.openiam.idm.srvc.res.dto.Resource;
 
 import javax.xml.bind.annotation.*;
@@ -10,11 +13,22 @@ import java.util.List;
  * Created by alexander on 02/08/15.
  */
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(name = "OAuthScopes", propOrder = {
+@XmlType(name = "OAuthScopesResponse", propOrder = {
+      "clientId",
       "oauthScopeList"
 })
-public class OAuthScopes implements Serializable {
+public class OAuthScopesResponse extends Response {
+    private String clientId;
     private List<Resource> oauthScopeList;
+
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
 
     public List<Resource> getOauthScopeList() {
         return oauthScopeList;
@@ -23,4 +37,5 @@ public class OAuthScopes implements Serializable {
     public void setOauthScopeList(List<Resource> oauthScopeList) {
         this.oauthScopeList = oauthScopeList;
     }
+
 }
