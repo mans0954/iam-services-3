932e24677329cfeda88ef5d701c53409da077433
Lev Bornovalov <lbornov2@gmail.com>
2015-11-24 23:38:23 +0100

    no need to reference DEV ENV

--- conf/setup.sh
+++ conf/setup.sh
@@ -10,9 +10,9 @@ sudo cp client.rb /etc/chef/client.rb
 sudo cp client.pem /etc/chef/client.pem
 sudo cp attributes.json /etc/chef/attributes.json
 sudo chmod 777 /opt/openiam/webapps
-sudo chef-client -o openiamapp::java8 -E DEV
-sudo chef-client -o openiam-properties::datasource -E DEV -j /etc/chef/attributes.json
-sudo chef-client -o openiam-properties::securityconf -E DEV -j /etc/chef/attributes.json
-sudo chef-client -o openiam-properties::service-urls -E DEV -j /etc/chef/attributes.json
-sudo chef-client -o openiam-conf -E DEV -j /etc/chef/attributes.json
-sudo chef-client -o openiam-hazelcast -E DEV -j /etc/chef/attributes.json
+sudo chef-client -o openiamapp::java8
+sudo chef-client -o openiam-properties::datasource -j /etc/chef/attributes.json
+sudo chef-client -o openiam-properties::securityconf -j /etc/chef/attributes.json
+sudo chef-client -o openiam-properties::service-urls -j /etc/chef/attributes.json
+sudo chef-client -o openiam-conf -j /etc/chef/attributes.json
+sudo chef-client -o openiam-hazelcast -j /etc/chef/attributes.json
