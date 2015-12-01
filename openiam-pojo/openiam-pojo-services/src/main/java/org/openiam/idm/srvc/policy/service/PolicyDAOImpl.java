db45d796698a64552ea8320ab6e382a808bce599
Dmitry Zaporozhec <Dmitry.Zaporozhec@openiam.com>
2015-12-01 18:33:02 +0300

    fix Policies orderBy

--- openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/policy/service/PolicyDAOImpl.java
+++ openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/policy/service/PolicyDAOImpl.java
@@ -76,9 +76,6 @@ public class PolicyDAOImpl extends BaseDaoImpl<PolicyEntity, String> implements
     @Override
     protected Criteria getExampleCriteria(final SearchBean searchBean, boolean isCount) {
         final Criteria criteria = getCriteria();
-        if (!isCount) {
-            criteria.addOrder(Order.asc("name")); //SIA 2015-08-01 Order by name
-        }
         if (searchBean instanceof PolicySearchBean) {
             PolicySearchBean sb = (PolicySearchBean) searchBean;
             if (StringUtils.isNotBlank(sb.getPolicyDefId())) {
