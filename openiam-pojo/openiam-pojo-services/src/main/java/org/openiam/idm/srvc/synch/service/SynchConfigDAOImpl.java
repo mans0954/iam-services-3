7e036b82b425acb6d6d79e39f9b6c1a062b7204b
Dmitry Zaporozhec <Dmitry.Zaporozhec@openiam.com>
2016-06-03 21:53:51 +0300

    Fix Sync Config get by example

--- openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/synch/service/SynchConfigDAOImpl.java
+++ openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/synch/service/SynchConfigDAOImpl.java
@@ -3,11 +3,13 @@ package org.openiam.idm.srvc.synch.service;
 import java.sql.Timestamp;
 import java.util.List;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.hibernate.Criteria;
 import org.hibernate.Query;
 import org.hibernate.criterion.Example;
+import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.openiam.core.dao.BaseDaoImpl;
@@ -23,102 +25,100 @@ import org.springframework.transaction.annotation.Transactional;
 @Repository("synchConfigDAO")
 public class SynchConfigDAOImpl extends BaseDaoImpl<SynchConfigEntity, String> implements SynchConfigDAO {
 
-	private static final Log log = LogFactory.getLog(SynchConfigDAOImpl.class);
+    private static final Log log = LogFactory.getLog(SynchConfigDAOImpl.class);
 
-	public SynchConfigEntity add(SynchConfigEntity transientInstance) {
-		try {
+    public SynchConfigEntity add(SynchConfigEntity transientInstance) {
+        try {
             getSession().persist(transientInstance);
-			return transientInstance;
-		} catch (RuntimeException re) {
-			log.error("persist failed", re);
-			throw re;
-		}
-	}
+            return transientInstance;
+        } catch (RuntimeException re) {
+            log.error("persist failed", re);
+            throw re;
+        }
+    }
 
-	public void remove(SynchConfigEntity persistentInstance) {
-		try {
+    public void remove(SynchConfigEntity persistentInstance) {
+        try {
             getSession().delete(persistentInstance);
-		} catch (RuntimeException re) {
-			log.error("delete failed", re);
-			throw re;
-		}
-	}
+        } catch (RuntimeException re) {
+            log.error("delete failed", re);
+            throw re;
+        }
+    }
 
-	public SynchConfigEntity merge(SynchConfigEntity detachedInstance) {
-		try {
+    public SynchConfigEntity merge(SynchConfigEntity detachedInstance) {
+        try {
             SynchConfigEntity ret = (SynchConfigEntity) getSession().merge(detachedInstance);
             return ret;
 
-		} catch (RuntimeException re) {
-			log.error("merge failed", re);
-			throw re;
-		}
-	}
+        } catch (RuntimeException re) {
+            log.error("merge failed", re);
+            throw re;
+        }
+    }
 
-	public SynchConfigEntity findById(java.lang.String id) {
+    public SynchConfigEntity findById(java.lang.String id) {
 
-		try {
-            SynchConfigEntity instance = (SynchConfigEntity)getCriteria()
+        try {
+            SynchConfigEntity instance = (SynchConfigEntity) getCriteria()
                     .add(Restrictions.idEq(id)).uniqueResult();
-			return instance;
-		} catch (RuntimeException re) {
-			log.error("get failed", re);
-			throw re;
-		}
-	}
+            return instance;
+        } catch (RuntimeException re) {
+            log.error("get failed", re);
+            throw re;
+        }
+    }
 
-	/* (non-Javadoc)
-	 * @see org.openiam.idm.srvc.synch.service.SynchConfigDAO#findAllConfig()
-	 */
-	public List<SynchConfigEntity> findAllConfig() {
+    /* (non-Javadoc)
+     * @see org.openiam.idm.srvc.synch.service.SynchConfigDAO#findAllConfig()
+     */
+    public List<SynchConfigEntity> findAllConfig() {
         Criteria criteria = getCriteria().addOrder(Order.asc("name"));
-        return (List<SynchConfigEntity>)criteria.list();
-	}
-	@Transactional
-	public int updateExecTime(String configId, Timestamp execTime) {
-		Query qry = getSession().createQuery("UPDATE org.openiam.idm.srvc.synch.domain.SynchConfigEntity sc " +
+        return (List<SynchConfigEntity>) criteria.list();
+    }
+
+    @Transactional
+    public int updateExecTime(String configId, Timestamp execTime) {
+        Query qry = getSession().createQuery("UPDATE org.openiam.idm.srvc.synch.domain.SynchConfigEntity sc " +
                 " 					SET  sc.lastExecTime = :execTime  " +
                 " 					WHERE  sc.id = :configId	");
-		
-		qry.setTimestamp("execTime", execTime);
-		qry.setString("configId", configId);
-		
-		return qry.executeUpdate();
-	}
-	@Transactional
-    public int updateLastRecProcessed(String configId,String processTime) {
-		Query qry = getSession().createQuery(" UPDATE org.openiam.idm.srvc.synch.domain.SynchConfigEntity sc " +
+
+        qry.setTimestamp("execTime", execTime);
+        qry.setString("configId", configId);
+
+        return qry.executeUpdate();
+    }
+
+    @Transactional
+    public int updateLastRecProcessed(String configId, String processTime) {
+        Query qry = getSession().createQuery(" UPDATE org.openiam.idm.srvc.synch.domain.SynchConfigEntity sc " +
                 " 					SET  sc.lastRecProcessed = :processTime  " +
                 " 					WHERE  sc.id = :configId	");
 
-		qry.setString("processTime",processTime);
-		qry.setString("configId", configId);
+        qry.setString("processTime", processTime);
+        qry.setString("configId", configId);
 
-		return qry.executeUpdate();
-	}
+        return qry.executeUpdate();
+    }
 
     @Override
     protected String getPKfieldName() {
         return "id";
     }
-    
-    
+
 
     @Override
-	protected Criteria getExampleCriteria(SearchBean searchBean) {
-		final Criteria criteria = getCriteria();
-		if(searchBean != null && searchBean instanceof SynchConfigSearchBean) {
-			final SynchConfigSearchBean sb = (SynchConfigSearchBean)searchBean;
-			if(sb.isExcludeBooleanProperties()) {
-				 final Example example = Example.create(new SynchConfigEntity());
-				 example.excludeProperty("usePolicyMap"); // exclude boolean properties
-				 example.excludeProperty("useTransformationScript");
-				 example.excludeProperty("policyMapBeforeTransformation");
-				 example.excludeProperty("useSystemPath");
-				 example.excludeProperty("searchScope");
-				 criteria.add(example);
-			}
-		}
-		return criteria;
-	}
+    protected Criteria getExampleCriteria(SearchBean searchBean) {
+        final Criteria criteria = getCriteria();
+        if (searchBean != null && searchBean instanceof SynchConfigSearchBean) {
+            final SynchConfigSearchBean sb = (SynchConfigSearchBean) searchBean;
+            if (StringUtils.isNotBlank(sb.getName())) {
+                criteria.add(Restrictions.ilike("name", sb.getName(), MatchMode.START));
+            }
+            if (StringUtils.isNotBlank(sb.getSynchType())) {
+                criteria.add(Restrictions.eq("synchType", sb.getSynchType()));
+            }
+        }
+        return criteria;
+    }
 }
