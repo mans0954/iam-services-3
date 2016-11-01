6ac406fffd479baf875bfe814fc213b25275014f
Vyacheslav Gnenny <vyacheslav.gnenny@openiam.com>
2016-11-02 02:46:24 +0300

    add methods to groupService

--- openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/grp/service/GroupAttributeDAOImpl.java
+++ openiam-pojo/openiam-pojo-services/src/main/java/org/openiam/idm/srvc/grp/service/GroupAttributeDAOImpl.java
@@ -2,9 +2,7 @@ package org.openiam.idm.srvc.grp.service;
 
 // Generated Jun 12, 2007 10:46:15 PM by Hibernate Tools 3.2.0.beta8
 
-import java.util.Collections;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 import javax.annotation.PostConstruct;
 import javax.naming.InitialContext;
@@ -69,4 +67,36 @@ public class GroupAttributeDAOImpl extends BaseDaoImpl<GroupAttributeEntity, Str
 	public List<GroupAttributeEntity> findGroupAttributes(String groupId) {
 		return (List<GroupAttributeEntity>)getCriteria().add(Restrictions.eq("group.id",groupId)).addOrder(Order.asc("name")).list();
 	}
+
+	@Override
+	public List<Map<String,String>> getAttributeByGroupIds(List<String> groupIds, String attrName) {
+		String sql = new String("SELECT new map(ga.value as value, ge.id as id, ge.name as name) "
+				+ "from org.openiam.idm.srvc.grp.domain.GroupAttributeEntity ga, org.openiam.idm.srvc.grp.domain.GroupEntity ge "
+				+ "where ga.group.id=ge.id and ge.id in (:groupIds) ");
+		Session session = getSession();
+		Query qry = session.createQuery(sql);
+		qry.setParameterList("groupIds", groupIds);
+		log.info("sql = " + qry.toString());
+
+		List<Map<String,String>> results = (List<Map<String,String>>) qry.setCacheable(this.cachable()).list();
+		return results;
+	}
+
+	@Override
+	public String getAttributeByGroupId(String groupId, String attrName){
+		String sql = new String("SELECT ga.value as value "
+				+ "from org.openiam.idm.srvc.grp.domain.GroupAttributeEntity ga "
+				+ "where ga.group.id=:groupIds ");
+		Session session = getSession();
+		Query qry = session.createQuery(sql);
+		qry.setString("groupIds", groupId);
+
+		List<String> results = (List<String>) qry.setCacheable(this.cachable()).list();
+		if (results == null) {
+			return null;
+		}
+		return results.get(0);
+	}
+
+
 }
