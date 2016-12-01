d679325d47f17e2acfe31b6aa56ac2ad51a1e6a1
Dmitry Zaporozhec <Dmitry.Zaporozhec@openiam.com>
2016-12-01 21:03:30 +0300

    IDMAPPS-3990 Audit Log archive

--- openiam-pojo/openiam-pojo-intf/src/main/java/org/openiam/idm/srvc/audit/service/AuditLogService.java
+++ openiam-pojo/openiam-pojo-intf/src/main/java/org/openiam/idm/srvc/audit/service/AuditLogService.java
@@ -3,6 +3,7 @@ package org.openiam.idm.srvc.audit.service;
 import org.openiam.idm.searchbeans.AuditLogSearchBean;
 import org.openiam.idm.srvc.audit.dto.IdmAuditLog;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -12,12 +13,16 @@ import java.util.List;
 public interface AuditLogService {
 
     public void enqueue(final IdmAuditLog idmAuditLog);
-    
+
     public List<IdmAuditLog> findBeans(final AuditLogSearchBean searchBean, final int from, final int size, final boolean isDeep);
+
     public List<String> findIDs(final AuditLogSearchBean searchBean, final int from, final int size);
 
     public int count(final AuditLogSearchBean searchBean);
+
     public IdmAuditLog findById(final String id);
+
     public IdmAuditLog save(IdmAuditLog auditLogEntity);
 
+    public void deleteOlderThan(Date date);
 }
