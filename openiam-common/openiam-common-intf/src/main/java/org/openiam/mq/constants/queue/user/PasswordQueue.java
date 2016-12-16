16828c3692e445186b9c94b573eff50ce1efe29b
Alexander Duckardt <aduckardt@gmail.com>
2016-12-16 16:19:53 -0500

    fix v_host for password web service

--- openiam-common/openiam-common-intf/src/main/java/org/openiam/mq/constants/queue/user/PasswordQueue.java
+++ openiam-common/openiam-common-intf/src/main/java/org/openiam/mq/constants/queue/user/PasswordQueue.java
@@ -1,11 +1,9 @@
 package org.openiam.mq.constants.queue.user;
 
-import org.openiam.mq.constants.queue.common.CommonQueue;
-
 /**
  * Created by alexander on 17/11/16.
  */
-public class PasswordQueue extends CommonQueue {
+public class PasswordQueue extends UserQueue {
     public PasswordQueue() {
         super(PasswordQueue.class.getSimpleName());
     }
