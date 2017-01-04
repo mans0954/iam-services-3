c2c0fc86303e469f8e432f95b7cacf317b2455ed
Lev Bornovalov <lbornov2@gmail.com>
2017-01-04 18:06:31 +0100

    AM-1127
    Update to latest Spring Boot, ElasticSearch, and Hibernate

--- conf/setup.sh
+++ conf/setup.sh
@@ -27,8 +27,6 @@ sudo chef-client -o openiam-devops-hazelcast -j /etc/chef/attributes.json
 wget https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/tar/elasticsearch/2.4.3/elasticsearch-2.4.3.tar.gz
 tar -xvf elasticsearch-2.4.3.tar.gz
 sudo cp elasticsearch.yml elasticsearch-2.4.3/config/elasticsearch.yml
-elasticsearch-2.4.3/bin/elasticsearch: {background: true}
-sleep 10 && wget --waitretry=5 --retry-connrefused -v http://127.0.0.1:9200/
 
 sudo chmod 777 /data/openiam/logs
 sudo chown ubuntu /data/openiam/logs
